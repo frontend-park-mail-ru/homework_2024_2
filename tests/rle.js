38a3d491c71397aa57f8715dee0c27a0813232f3
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2019-02-04 13:28:04 +0300

    Подготовка варианта 5

--- tests/rle.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-QUnit.module('Тестируем функцию rle', function () {
-	QUnit.test('rle работает правильно', function (assert) {
-		assert.strictEqual(rle('AAAB'), 'A3B');
-		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
-		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
-	});
-});
