f8c0eb30489b7e776a86b6bad5be8c020cab55b8
Alexeyzem <Alex.zem2015@mail.ru>
2024-08-31 20:53:32 +0300

    add ';' in end of line

--- source/sort.js
+++ source/sort.js
@@ -1,29 +1,29 @@
 'use strict';
 
-const collator = new Intl.Collator()
+const collator = new Intl.Collator();
 
 
 const sort = (sentence) => {
     if (typeof(sentence) !== 'string') {
-        return ""
+        return "";
     }
     sentence=sentence.trim()
     if (sentence.length === 0){
-        return ""
+        return "";
     }
-    let str = sentence.toLowerCase()
-    let words = sortLetterInWords(str.split(" "))
-    words.sort(collator.compare)
-    return words.join(" ")
+    let str = sentence.toLowerCase();
+    let words = sortLetterInWords(str.split(" "));
+    words.sort(collator.compare);
+    return words.join(" ");
 }
 
 function sortLetterInWords(words){
     for (let i in words) {
-        let letterArray = words[i].split("")
-        letterArray.sort(collator.compare)
-            words[i] = letterArray[0].toUpperCase() + letterArray.join("").slice(1)
+        let letterArray = words[i].split("");
+        letterArray.sort(collator.compare);
+            words[i] = letterArray[0].toUpperCase() + letterArray.join("").slice(1);
     }
-    return words
+    return words;
 }
 
 
