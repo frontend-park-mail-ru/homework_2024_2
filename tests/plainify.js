d97fdf1454d00cdc98abc9df7d41dade9683649d
AlexeyBMSTU <nap8099@gmail.com>
2024-08-31 13:30:06 +0300

    Поправил

--- tests/plainify.js
+++ tests/plainify.js
@@ -1,57 +1,90 @@
-// Здравствуйте, я Алексей Нарыжный из группы WEB-21. 
-// Мои контакты:
-// tg: fish190 | vk: vk.com/leshka_055
-
 'use strict';
 
-// Определяем функцию plainify, которая принимает объект
-const plainify = (obj, prefix = '') =>
-
-  // Используем Object.entries для итерации по ключ-значению объекта (хеш)
-  Object.entries(obj).reduce((acc, [key, value]) => {
-
-    // Создаем новый ключ, конкатенируя префикс и оригинальный ключ
-    const newKey = prefix ? `${prefix}.${key}` : key;
-
-    // Если значение является объектом, рекурсивно вызываем plainify
-    return typeof value === 'object'
-      ? { ...acc, ...plainify(value, newKey) }
-
-      // Иначе, добавляем значение с новым ключом
-      : { ...acc, [newKey]: value };
-  }, {});
-
-// Модуль тестирования функции plainify
 QUnit.module('Тестируем функцию plainify', function () {
+	QUnit.test('Преобразование вложенных объектов в плоские', function (assert) {
+		assert.deepEqual(plainify({foo: 'bar', baz: 42}), {'foo': 'bar', 'baz': 42});
 
-  // Тест: plainify работает правильно
-  QUnit.test('plainify работает правильно', function (assert) {
+		const nested1 = {
+			deep: {
+				foo: 'bar',
+				baz: 42
+			}
+		};
 
-    // Проверяем, что plainify работает корректно для простого объекта
-    assert.deepEqual(plainify({ foo: 'bar', baz: 42 }), { foo: 'bar', baz: 42 });
+		const plain1 = {
+			'deep.foo': 'bar',
+			'deep.baz': 42
+		};
 
-    // Проверяем, что plainify работает корректно для вложенного объекта
-    assert.deepEqual(plainify({ deep: { foo: 'bar', baz: 42 } }), { 'deep.foo': 'bar', 'deep.baz': 42 });
-    
-  // Проверяем, что plainify работает корректно для сложного вложенного объекта
-    assert.deepEqual(plainify({
-      deep: {
-        foobar: 0,
-        nested: {
-          object: {
-            fields: {
-              foo: 42,
-              bar: 42,
-              baz: 42
-            }
-          }
-        }
-      }
-    }), {
-      'deep.foobar': 0,
-      'deep.nested.object.fields.foo': 42,
-      'deep.nested.object.fields.bar': 42,
-      'deep.nested.object.fields.baz': 42
-    });
+		assert.deepEqual(plainify(nested1), plain1);
+
+		const nested2 = {
+			deep: {
+				foobar: 0,
+				nested: {
+					object: {
+						fields: {
+							foo: 42,
+							bar: 42,
+							baz: 42
+						}
+					}
+				}
+			}
+		};
+
+		const plain2 = {
+			'deep.foobar': 0,
+			'deep.nested.object.fields.foo': 42,
+			'deep.nested.object.fields.bar': 42,
+			'deep.nested.object.fields.baz': 42
+		};
+
+		assert.deepEqual(plainify(nested2), plain2);
+	});
+
+  QUnit.test('Преобразование не объекта', function (assert) {
+    assert.deepEqual(plainify('строка'), {}, 'Строка должна преобразоваться в пустой объект');
   });
+  
+  QUnit.test('Преобразование объекта с null', function (assert) {
+    const obj = { 
+      foo: null, 
+      bar: 42 
+    };
+
+    const expected = { 
+      foo: null, 
+      bar: 42 
+    };
+
+    assert.deepEqual(plainify(obj), expected, 'Null должно остаться null');
+  });
+  
+  QUnit.test('Преобразование объекта с массивом', function (assert) {
+    const obj = { 
+      foo: [1, 2, 3], 
+      bar: 42 
+    };
+
+    const expected = { 
+      foo: [1, 2, 3], 
+      bar: 42 
+    };
+
+    assert.deepEqual(plainify(obj), expected, 'Массив должен остаться массивом');
+  });
+  
+  QUnit.test('Преобразование объекта с префиксом, не являющимся строкой', function (assert) {
+    const obj = { 
+      foo: 'bar' 
+    };
+
+    const prefix = 42;
+
+    assert.throws(function () {
+      plainify(obj, prefix);
+    }, 'Префикс должен быть строкой'); 
+  });
+
 });
