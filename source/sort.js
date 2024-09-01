1989d2fed8b333da4f630d327091db6ebcec2df4
Alexeyzem <Alex.zem2015@mail.ru>
2024-09-01 19:25:18 +0300

    add mySort function

--- source/sort.js
+++ source/sort.js
@@ -10,16 +10,34 @@ const sort = (sentence) => {
     }
     let str = sentence.toLowerCase().replaceAll('ё', 'е');
     let words = sortLetterInWords(str.split(" "));
-    words.sort();
+    words = mySort(words)
     return words.join(" ");
 }
 
 let sortLetterInWords = (words) => {
     words.forEach((word, i)=> {
         let letterArray = word.split("");
-        letterArray.sort();
+        letterArray = mySort(letterArray)
         words[i] = letterArray[0].toUpperCase() + letterArray.join("").slice(1);
     });
     return words;
+}
+
+const mySort = (array) => {
+   if (array.length <= 1){
+       return array;
+   }
+   let pivot = array[0];
+   let leftArr = [];
+   let rightArr = [];
+    array = array.slice(1)
+    for (const value of array) {
+        if (value < pivot){
+            leftArr.push(value);
+        } else {
+            rightArr.push(value);
+        }
+    }
+    return [...mySort(leftArr), pivot, ...mySort(rightArr)];
 }
 
