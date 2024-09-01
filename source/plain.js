e811e6d435d4992ecd5bcc75a2da3cbda48e5598
Zhukov Georgiy <zhugeo@mail.ru>
2024-09-01 20:11:25 +0300

    Добавлены дополнительные тесты, в функции добавлена проверка на количество аргументов

--- source/plain.js
+++ source/plain.js
@@ -3,9 +3,14 @@
 /** Принимает на вход массив массивов и создаёт из них один общий массив. Массивы могут быть любой вложенности
  * @param {any[]} array
  */
-const plain = (array) => {
+const plain = function (array) {
   // Проверка на то, является ли аргумент массивом
   if (!Array.isArray(array)) throw new Error("Argument should be an array");
+  // Проверка на то, что передан только один аргумент
+  if (arguments.length == 0 || arguments.length > 1)
+    throw new Error(
+      `Function should get one argument, got ${arguments.length}`
+    );
   // Рекурсивно выпрямляем массив
   const ret = new Array();
   for (const elem of array) {
