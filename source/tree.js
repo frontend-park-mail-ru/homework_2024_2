23532616de6cdcf59273e568e5f2d29c470a6ae7
MatiXxD <ilya_dinesenko@mail.ru>
2024-09-02 23:38:11 +0300

    Little fixes

--- source/tree.js
+++ source/tree.js
@@ -2,24 +2,20 @@
 
 const tree = (treeHeight) => {
   const MIN_TREE_HEIGHT = 2;
-  const startsRowsCount = Math.trunc(Number(treeHeight)) - 1;
+  const starsRowsCount = Math.trunc(Number(treeHeight)) - 1;
 
-  if (
-    typeof treeHeight === "object" ||
-    !isFinite(treeHeight) ||
-    startsRowsCount + 1 <= MIN_TREE_HEIGHT
-  ) {
+  if (!isFinite(treeHeight) || starsRowsCount + 1 <= MIN_TREE_HEIGHT) {
     return null;
   }
 
   let treeString = "";
-  for (let i = 0; i != startsRowsCount ; ++i) {
+  for (let i = 0; i != starsRowsCount; ++i) {
     const spaces = " ".repeat(treeHeight - i - 2);
     const stars = "*".repeat(2 * i + 1);
     treeString += spaces + stars + spaces + "\n";
   }
 
-  const lastRowSpaces = " ".repeat(startsRowsCount - 1);
+  const lastRowSpaces = " ".repeat(starsRowsCount - 1);
   treeString += `${lastRowSpaces}|${lastRowSpaces}\n`;
 
   return treeString;
