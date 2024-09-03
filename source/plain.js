56fb5f2704b6779d7322666e2af3da0b84eb96ad
Zhukov Georgiy <zhugeo@mail.ru>
2024-09-03 23:43:46 +0300

    Сделаны правки

--- source/plain.js
+++ source/plain.js
@@ -3,7 +3,8 @@
 /** Принимает на вход массив массивов и создаёт из них один общий массив. Массивы могут быть любой вложенности
  * @param {any[]} array
  * @returns {any[]} Общий массив
- * @throws Выдаст ошибку, если аргумент - не массив или если передан не один аргумент
+ * @throws {Error} Выдаст ошибку, если передан не один аргумент
+ * @throws {TypeError} Выдаст ошибку, если передан не массив
  */
 function plain(array) {
   // Проверка на то, является ли аргумент массивом
@@ -18,13 +19,13 @@ function plain(array) {
   }
   // Рекурсивно выпрямляем массив
   const plainedArray = new Array();
-  for (const elem of array) {
+  array.forEach((el) => {
     if (Array.isArray(elem)) {
       const plainElem = plain(elem);
       plainElem.forEach((elem) => plainedArray.push(elem));
     } else {
       plainedArray.push(elem);
     }
-  }
+  });
   return plainedArray;
 }
