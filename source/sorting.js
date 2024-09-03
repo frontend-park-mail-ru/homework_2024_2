192a69a37249446de444e2e1d54ea69b46e98d90
Valentin <v_stremin@mail.ru>
2024-09-03 19:08:56 +0300

    Переписана функция sorting без использования встроенной функции sort

--- source/sorting.js
+++ source/sorting.js
@@ -1,10 +1,30 @@
 "use strict";
 
-const sorting = (arr, props) =>
-  arr.sort((a, b) => {
-    for (let prop of props) {
-      if (a[prop] < b[prop]) return -1;
-      if (a[prop] > b[prop]) return 1;
+const sorting = (arr, props) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const sortedArr = [...arr];
+
+  for (let i = 0; i < sortedArr.length - 1; i++) {
+    for (let j = 0; j < sortedArr.length - 1 - i; j++) {
+      let flag = false;
+
+      for (let key of props) {
+        if (sortedArr[j][key] > sortedArr[j + 1][key]) {
+          flag = true;
+          break;
+        } else if (sortedArr[j][key] < sortedArr[j + 1][key]) {
+          break;
+        }
+      }
+
+      if (flag) {
+        [sortedArr[j], sortedArr[j + 1]] = [sortedArr[j + 1], sortedArr[j]];
+      }
     }
-    return 0;
-  });
+  }
+
+  return sortedArr;
+};
