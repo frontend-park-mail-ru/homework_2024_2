81229cda520225ee4c70ecf3ebcbe74f2853fe8f
wiseStann <servicename18@gmail.com>
2024-09-04 18:49:33 +0300

    fixes and minor improvements

--- source/tree.js
+++ source/tree.js
@@ -8,31 +8,30 @@ const STARS_PER_LINE_INCREM_FACTOR = 2;
  * высота должна иметь либо строковый тип, либо целочисленный тип,
  * а так же иметь значение не меньше MIN_TREE_LAYERS_COUNT
  * 
- * @param {number} height 
+ * @param {(number|string)} height 
  * 
  * @returns {boolean} Результат проверки значения высоты дерева
  */
 const heightIsCorrect = (height) => {
-    return (typeof height === "number" && !isNaN(height) &&
-            Number.isInteger(height) || typeof height === "string") &&
-            height >= MIN_TREE_LAYERS_COUNT;
+    const heightInt = parseFloat(height);
+    return Number.isInteger(heightInt) && height >= MIN_TREE_LAYERS_COUNT;
 }
 
 /**
  * Возвращает ASCII-ёлочку заданной высоты, состоящую из звёздочек
  * 
- * @param {number} height 
+ * @param {(number|string)} height 
+ * 
+ * @throws {TypeError} для некорректного значения параметра
  * 
  * @returns {string} ASCII-ёлочка из звёздочек
- * 
- * @throws TypeError для некорректного значения параметра
  */
 const tree = (height) => {
     if (!heightIsCorrect(height)) {
-        throw new TypeError("Please provide a valid value for the tree height!");
+        throw new TypeError('Please provide a valid value for the tree height!');
     }
 
-    let resultTree = "";
+    let resultTree = '';
     const width = (height - 1) * STARS_PER_LINE_INCREM_FACTOR - 1;
     const center = Math.floor(width / 2);
 
@@ -42,21 +41,21 @@ const tree = (height) => {
             if (i === height - 1) {
                 // Палочка
                 if (j === center) {
-                    resultTree += "|";
+                    resultTree += '|';
                 } else {
-                    resultTree += " ";
+                    resultTree += ' ';
                 }
                 continue;
             }
             // Звездчоки
             if (center - i <= j && j <= center + i) {
-                resultTree += "*";
+                resultTree += '*';
                 continue;
             }
             // Иначе пробел
-            resultTree += " ";
+            resultTree += ' ';
         }
-        resultTree += "\n";
+        resultTree += '\n';
     }
 
     return resultTree;
