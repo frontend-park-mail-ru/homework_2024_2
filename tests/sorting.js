76059ecb4c234f6599b15a9024a4edbee4d69c45
Valentin <v_stremin@mail.ru>
2024-09-05 13:54:23 +0300

    Вернул табы в тестах

--- tests/sorting.js
+++ tests/sorting.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 
-QUnit.module("Тестируем функцию sorting", function () {
-  QUnit.test("sorting не меняет пустой массив", function (assert) {
-    const initial = [];
-    const actual = sorting(initial, []);
+QUnit.module('Тестируем функцию sorting', function () {
+	QUnit.test('sorting не меняет пустой массив', function (assert) {
+		const initial = [];
+		const actual = sorting(initial, []);
 
-    const expected = [];
+		const expected = [];
 
-    assert.deepEqual(actual, expected);
-  });
+		assert.deepEqual(actual, expected);
+	});
 
-  // мой тест 1
-  QUnit.test(
+   // мой тест 1
+   QUnit.test(
     "sorting корректно сортирует объекты с отрицательными числовыми свойствами",
     function (assert) {
       const initial = [
@@ -60,113 +60,114 @@ QUnit.module("Тестируем функцию sorting", function () {
     }
   );
 
-  QUnit.test(
-    "sorting не изменяет массив, если не передано никаких полей для сортировки",
-    function (assert) {
-      const initial = [{ prop1: 1 }, { prop1: 2 }, { prop1: 3 }, { prop1: 4 }];
-      const actual = sorting(initial, []);
+	QUnit.test('sorting не изменяет массив, если не передано никаких полей для сортировки', function (assert) {
+		const initial = [
+			{prop1: 1},
+			{prop1: 2},
+			{prop1: 3},
+			{prop1: 4}
+		];
+		const actual = sorting(initial, []);
 
-      const expected = [{ prop1: 1 }, { prop1: 2 }, { prop1: 3 }, { prop1: 4 }];
+		const expected = [
+			{prop1: 1},
+			{prop1: 2},
+			{prop1: 3},
+			{prop1: 4}
+		];
 
-      assert.deepEqual(actual, expected);
-    }
-  );
+		assert.deepEqual(actual, expected);
+	});
 
-  QUnit.test(
-    "sorting сортирует массив по численному свойству",
-    function (assert) {
-      const initial = [
-        { prop1: 30 },
-        { prop1: 1000 },
-        { prop1: 4 },
-        { prop1: 200 },
-      ];
-      const actual = sorting(initial, ["prop1"]);
+	QUnit.test('sorting сортирует массив по численному свойству', function (assert) {
+		const initial = [
+			{prop1: 30},
+			{prop1: 1000},
+			{prop1: 4},
+			{prop1: 200}
+		];
+		const actual = sorting(initial, [ 'prop1' ]);
 
-      const expected = [
-        { prop1: 4 },
-        { prop1: 30 },
-        { prop1: 200 },
-        { prop1: 1000 },
-      ];
+		const expected = [
+			{prop1: 4},
+			{prop1: 30},
+			{prop1: 200},
+			{prop1: 1000}
+		];
 
-      assert.deepEqual(actual, expected);
-    }
-  );
+		assert.deepEqual(actual, expected);
+	});
 
-  QUnit.test(
-    "sorting сортирует массив по строковому свойству",
-    function (assert) {
-      const initial = [
-        { prop1: "30" },
-        { prop1: "1000" },
-        { prop1: "4" },
-        { prop1: "200" },
-      ];
-      const actual = sorting(initial, ["prop1"]);
+	QUnit.test('sorting сортирует массив по строковому свойству', function (assert) {
+		const initial = [
+			{prop1: '30'},
+			{prop1: '1000'},
+			{prop1: '4'},
+			{prop1: '200'}
+		];
+		const actual = sorting(initial, [ 'prop1' ]);
 
-      const expected = [
-        { prop1: "1000" },
-        { prop1: "200" },
-        { prop1: "30" },
-        { prop1: "4" },
-      ];
+		const expected = [
+			{prop1: '1000'},
+			{prop1: '200'},
+			{prop1: '30'},
+			{prop1: '4'}
+		];
 
-      assert.deepEqual(actual, expected);
-    }
-  );
+		assert.deepEqual(actual, expected);
+	});
 
-  QUnit.test("sorting реализует устойчивую сортировку", function (assert) {
-    const initial = [
-      { prop1: 3, id: 1 },
-      { prop1: 3, id: 2 },
-      { prop1: 1, id: 1 },
-      { prop1: 1, id: 2 },
-      { prop1: 4, id: 1 },
-      { prop1: 4, id: 2 },
-      { prop1: 2, id: 1 },
-      { prop1: 2, id: 2 },
-    ];
-    const actual = sorting(initial, ["prop1"]);
+	QUnit.test('sorting реализует устойчивую сортировку', function (assert) {
+		const initial = [
+			{prop1: 3, id: 1},
+			{prop1: 3, id: 2},
+			{prop1: 1, id: 1},
+			{prop1: 1, id: 2},
+			{prop1: 4, id: 1},
+			{prop1: 4, id: 2},
+			{prop1: 2, id: 1},
+			{prop1: 2, id: 2}
+		];
+		const actual = sorting(initial, [ 'prop1' ]);
 
-    const expected = [
-      { prop1: 1, id: 1 },
-      { prop1: 1, id: 2 },
-      { prop1: 2, id: 1 },
-      { prop1: 2, id: 2 },
-      { prop1: 3, id: 1 },
-      { prop1: 3, id: 2 },
-      { prop1: 4, id: 1 },
-      { prop1: 4, id: 2 },
-    ];
+		const expected = [
+			{prop1: 1, id: 1},
+			{prop1: 1, id: 2},
+			{prop1: 2, id: 1},
+			{prop1: 2, id: 2},
+			{prop1: 3, id: 1},
+			{prop1: 3, id: 2},
+			{prop1: 4, id: 1},
+			{prop1: 4, id: 2}
+		];
 
-    assert.deepEqual(actual, expected);
-  });
+		assert.deepEqual(actual, expected);
+	});
 
-  QUnit.test("sorting сортирует по нескольким полям", function (assert) {
-    const initial = [
-      { prop1: 3, id: "1" },
-      { prop1: 3, id: "2" },
-      { prop1: 1, id: "1" },
-      { prop1: 1, id: "2" },
-      { prop1: 4, id: "1" },
-      { prop1: 4, id: "2" },
-      { prop1: 2, id: "1" },
-      { prop1: 2, id: "2" },
-    ];
-    const actual = sorting(initial, ["id", "prop1"]);
+	QUnit.test('sorting сортирует по нескольким полям', function (assert) {
+		const initial = [
+			{prop1: 3, id: '1'},
+			{prop1: 3, id: '2'},
+			{prop1: 1, id: '1'},
+			{prop1: 1, id: '2'},
+			{prop1: 4, id: '1'},
+			{prop1: 4, id: '2'},
+			{prop1: 2, id: '1'},
+			{prop1: 2, id: '2'}
+		];
+		const actual = sorting(initial, [ 'id', 'prop1' ]);
 
-    const expected = [
-      { prop1: 1, id: "1" },
-      { prop1: 2, id: "1" },
-      { prop1: 3, id: "1" },
-      { prop1: 4, id: "1" },
-      { prop1: 1, id: "2" },
-      { prop1: 2, id: "2" },
-      { prop1: 3, id: "2" },
-      { prop1: 4, id: "2" },
-    ];
+		const expected = [
+			{prop1: 1, id: '1'},
+			{prop1: 2, id: '1'},
+			{prop1: 3, id: '1'},
+			{prop1: 4, id: '1'},
+			{prop1: 1, id: '2'},
+			{prop1: 2, id: '2'},
+			{prop1: 3, id: '2'},
+			{prop1: 4, id: '2'}
+		];
 
-    assert.deepEqual(actual, expected);
-  });
+		assert.deepEqual(actual, expected);
+	});
 });
