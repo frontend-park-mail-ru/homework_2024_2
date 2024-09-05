0ccb92cfe4bbe15d0d95f6dd732d89939cb94d08
Valentin <v_stremin@mail.ru>
2024-09-06 00:02:54 +0300

    Добавил JSDoc

--- source/sorting.js
+++ source/sorting.js
@@ -1,6 +1,34 @@
 "use strict";
 
+/** Принимает на вход массив plain-объектов и массив свойств, по которым необходимо отсортировать массив объектов. Реализует устойчивую сортировку этого массива в порядке возрастания.
+ *
+ * @param {object[]} arr массив plain-объектов
+ * @param {string[]} props массив свойств, по которым необходимо отсортировать массив
+ * @returns {object[]} Отсортированный массив
+ * @throws {TypeError} Выдаст ошибку, если один из аргументов не передан
+ * @throws {TypeError} Выдаст ошибку, если arr это не массив
+ * @throws {TypeError} Выдаст ошибку, если props это не массив
+ * @throws {TypeError} Выдаст ошибку, если хотя бы один элемент arr не является объектом
+ * @throws {TypeError} Выдаст ошибку, если хотя бы один объект не содержит свойство, по которому arr сортируется
+ */
+
 const sorting = (arr, props) => {
+  if (arr === undefined || props === undefined)
+    throw new TypeError("Either arr or props is undefined");
+  if (!Array.isArray(arr)) throw TypeError("arr should be array");
+  if (!Array.isArray(props)) throw TypeError("props should be array");
+  arr.forEach((elem) => {
+    if (typeof elem !== "object") {
+      throw TypeError(`arr should contain objects only, not ${typeof elem}`);
+    }
+    props.forEach((prop) => {
+      if (elem[prop] === undefined) {
+        throw new Error(
+          `At least one plain object in arr doesnt contain orderby prop ${prop}`
+        );
+      }
+    });
+  });
   if (arr.length <= 1) {
     return arr;
   }
