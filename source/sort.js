4436ebed50ef6d92f5f857d7775eaa50fa856193
Stas <stas_kriger@mail.ru>
2024-09-06 17:46:24 +0300

    fixed

--- source/sort.js
+++ source/sort.js
@@ -6,17 +6,23 @@
     * Если параметр не строка, то выдается ошибка RuntimeError с сообщением - 'Некорректные данные'   
     * @returns {string} Строку с отсортированными буквами в словах и отсортированными словами в строке, с первой прописной и остальными строчными бувами в каждом слове 
 */
-const sort = function (str){
-    if (typeof(str) != 'string') throw new RuntimeError('Некорректные данные');
+const sort = (str) =>{
+    if (typeof(str) != 'string'){
+        throw new RuntimeError('Некорректные данные');
+    }
 
-    let arrayOfStrings = str.split(' ');
-    arrayOfStrings.forEach(function(item, i, arrayOfStrings){
-        arrayOfStrings[i] = arrayOfStrings[i].toLowerCase().split('').sort((a, b) => a.localeCompare(b)).join('');
-        if (arrayOfStrings[i]) arrayOfStrings[i] = arrayOfStrings[i][0].toUpperCase() + arrayOfStrings[i].slice(1);
-       
-    })
-    arrayOfStrings.sort((a, b) => a.localeCompare(b));
+    const arrayOfStrings = str.split(' ');
+    const result = arrayOfStrings.reduce( (result_array_string, item) => {
+        
+        item = item.toLowerCase().split('').sort((a, b) => a.localeCompare(b)).join('');
+        if (item){
+            item = item[0].toUpperCase() + item.slice(1);
+        }
+        
+        result_array_string.push(item);
+        return result_array_string;
+        
+    }, []);
 
-    return arrayOfStrings.join(' ');
-    
+    return result.sort((a, b) => a.localeCompare(b)).join(' ');
 };
