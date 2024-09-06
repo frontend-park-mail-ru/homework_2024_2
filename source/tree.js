55d5db34421747232c671e2fa4b5ee27621487ec
PtFux <polina.tikunowa@gmail.com>
2024-09-06 12:29:17 +0300

    Update tree.js

--- source/tree.js
+++ source/tree.js
@@ -1,32 +1,27 @@
 'use strict';
 
+
 function tree(a) {
-    // alert( 'Всем привет!' );
-    if (a  < 3){
-        return null;
-    } else {
-        var result = "";
-        var crone = "*";
-        let i = 0;
+    if (a < 3)
+      return null;
+    
+    let result = "";
+    let crone = "*";
+    let i = 0;
 
-        while (i < a) { // выводит 0, затем 1, затем 2
-        // alert( i );
-        
-        if (i == a - 1) {
-            var empty = " ".repeat((2*(a - 2)) / 2);
-            result = result + empty + "|" + empty + "\n";
-        } else {
-            let k = (2*(a - 2) - i * 2) / 2;
-            var empty = ' '.repeat(k);
-            result = result + empty + crone + empty + '\n';
-            crone = crone + "**";
-        }
-        i++;
-        }
-
-        return result;
-
+    while (i < a) { 
+    
+      if (i === a - 1) {
+        const empty = " ".repeat( (2*(a - 2)) / 2 );
+        result += empty + "|" + empty + "\n";
+      } else {
+        let k = ( 2 * (a - 2) - i * 2 ) / 2;
+        const empty = ' '.repeat(k);
+        result += empty + crone + empty + '\n';
+        crone = crone + "**";
+      }
+      i++;
     }
+
+    return result;
   }
-  
-// const tree = nill;
