0b696b6dd63d0b0590914111fdc294e557d3b3a8
oompa-Loompa <57658482+Danil-Zlo@users.noreply.github.com>
2024-09-06 18:30:01 +0300

    Add files via upload

--- source/euclid.js
+++ source/euclid.js
@@ -1,21 +1,30 @@
 "use strict";
 
-function euclid(...numbers) {
-  // Проверка на отсутствие символа в массиве  
-  if (!numbers.every(Number.isInteger)) {
-    return 'Error';
+/**
+ * Вычисляет наибольший общий делитель (НОД) двух чисел с помощью алгоритма Евклида.
+ * @param {number} a Первое число.
+ * @param {number} b Второе число.
+ * @returns {number} НОД чисел a и b.
+ */
+function findNOD(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return a;
+}
+
+/**
+ * Вычисляет НОД нескольких чисел с помощью алгоритма Евклида.
+ * @param {...number} числа Массив чисел.
+ * @returns {number} НОД всех чисел в массиве.
+ */
+let euclid = (...numbers) => {
+  if (!numbers.length) {
+    throw new Error('No arguments provided');
   }
 
-  let result = numbers[0];
-  for (let i = 1; i < numbers.length; i++) {
-    let a = result;
-    let b = numbers[i];
-    while (b !== 0) {
-      let temp = b;
-      b = a % b;
-      a = temp;
-    }
-    result = a;
+  if (!numbers.every(Number.isInteger)) {
+    throw new Error('euclid() function does not accept non-numeric values')
   }
-  return result;
+  return numbers.reduce((a, b) => findNOD(a, b), numbers[0]);
 }
