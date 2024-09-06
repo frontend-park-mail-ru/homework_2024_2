73f9f7961c2a9168b16a08413b7bad332459b817
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-09-06 12:52:10 +0300

    Добавлена реализация быстрой сортировки, sorting теперь не меняет исходный массив

--- source/sorting.js
+++ source/sorting.js
@@ -1,11 +1,32 @@
 'use strict';
 
 const sorting = (initial, fields) => {
+    let sorted = initial.slice();
     fields.reverse().forEach(field => {
-        initial.sort((a, b) => {
-            if (a[field] === b[field]) return 0;
+        sorted = quickSort(sorted, (a, b) => {
+            if (a[field] === b[field]) {
+                return 0;
+            }
             return a[field] < b[field] ? -1 : 1;
         });
-    })
-    return initial;
-}
+    });
+    return sorted;
+};
+
+const quickSort = (arr, callback) => {
+    if (arr.length <= 1) {
+        return arr;
+    };
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (callback(arr[i], pivot) < 0) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left, callback).concat(pivot, quickSort(right, callback));
+};
