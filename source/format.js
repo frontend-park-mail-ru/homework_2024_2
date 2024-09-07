b1906a3c09bc711a24c66ea7ee815039265d4ec3
Olgmuzalev13 <121461949+Olgmuzalev13@users.noreply.github.com>
2024-09-07 18:58:31 +0300

    Update format.js

--- source/format.js
+++ source/format.js
@@ -1,16 +1,30 @@
-function format(input, columns) {
-  let output = Array(10).fill("");
-  for (let j=0;j<columns;j++){
-    listt=[]
-    for (let i=j;i<input.length;i+=columns){
-      listt.push(input[i])
-      console.log(input[i],String(input[i]).length)
-    }
-    max_len = Math.max(String(Math.max.apply(null, listt)).length, String(Math.min.apply(null, listt)).length)
-    listt.forEach((element, iter) => {listt[iter]=" ".repeat(max_len-String(element).length)+element});
-    console.log("@", listt)
-    listt.forEach((element, iter) => {output[iter]+=" "+" ".repeat(max_len-String(element).length)+element});
-    
-  }
-  return output.join("\n")
+'use strict';
+
+let format = (input, columns) => {
+	if (columns < 1) {
+		return `Amount of coloms could't be ${columns}`
+	}
+	if (columns > input.length) {
+		return `Amount of coloms could't be bigger than amount of numbers (${columns}>${input.length})`
+	}
+	let output = Array(Math.ceil(input.length / columns)).fill("");
+	for (let j = 0; j < columns; j++) {
+		let lst = []
+		let maxLen = 0
+		for (let i = j; i < input.length; i += columns) {
+			if (typeof (input[i]) != "number") {
+				return `${typeof (input[i])} is in input data, all elements must be numbers`
+			}
+			lst.push(input[i])
+			if (String(input[i]).length > maxLen) {
+				maxLen = String(input[i]).length
+			}
+		}
+		lst.forEach((element, iter) => {
+			output[iter] += (output[iter] == "" ? "" : " ")
+				+ " ".repeat(maxLen - String(element).length) + element
+		});
+	}
+	return output.join("\n")
 }
+
