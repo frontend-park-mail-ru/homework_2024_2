d6e91e0db1efc9e4f46e660b64f6d2f94153ed94
Ilya Andriyanov <ilyaandry35@gmail.com>
2024-09-07 19:49:13 +0300

    fix: zip function documentation

--- source/zip.js
+++ source/zip.js
@@ -5,8 +5,6 @@
  * some of the objects have same attribute, value of the first object with
  * such an attribute will be taken.
  * Note that subobjects are not deepcopied.
- * If the function receives at least one non-object argument,
- * the output will be undefined.
  * @param {...Object} objects - The objects to be zipped
  * @throws {TypeError} All of the arguments must be objects
  */
