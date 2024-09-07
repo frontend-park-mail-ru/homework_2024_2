4a6d1c1c25be1296fcf137f8b9790ec0480832ee
nonrep <seld210@mail.ru>
2024-09-07 17:25:02 +0300

    added error messages and object support

--- source/solve.js
+++ source/solve.js
@@ -1,15 +1,53 @@
 'use strict';
 
-function solve(formula, x) {
+const solve = (formula, x) => {
+    const formulaErrorMessage = 'The expression must consist only of addition, subtraction, and multiplication operations, using only integers and parentheses'
+    const variableErrorMessage = 'x must be a number or a Number object'
+    const infinityVariableErrorMessage = 'x is too large'
+    const infinityResultErrorMessage = 'Result is too large' 
+
+    if (formula instanceof String) {
+        formula = formula.toString()
+    }
+
+    if (typeof x === 'object' && x instanceof Number) {
+        x = x.valueOf()
+    }
+
     if (typeof x !== 'number') {
-        return null;
+        throw new Error(variableErrorMessage);
+    }
+
+    if (x === Infinity || x === -Infinity) {
+        throw new Error(infinityVariableErrorMessage)
     }
 
     try {
         const validation = /^(?!.*\*\*)([0-9x()*\s-+]+)$/
-        const result = validation.test(formula) ? eval(formula) : null
+
+        if (!validation.test(formula)) {
+            throw new Error(formulaErrorMessage);
+        }
+
+        const result = eval(formula)
+
+        if (result === Infinity || result === -Infinity) {
+            throw new Error(infinityResultErrorMessage);
+        }
+
         return result    
     } catch (error) {
-        return null;
+        const allowedMessages = [
+            formulaErrorMessage,
+            variableErrorMessage,
+            infinityVariableErrorMessage,
+            infinityResultErrorMessage
+        ]
+    
+        const message = allowedMessages.includes(error.message) 
+            ? error.message 
+            : 'Error in expression'
+
+        throw new Error(message);
     }
 }
--- tests/solve.js
+++ tests/solve.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const formulaErrorMessage = /The expression must consist only of addition, subtraction, and multiplication operations, using only integers and parentheses/
+const variableErrorMessage = /x must be a number or a Number object/
+const defaultErrorMessage = /Error in expression/
+const infinityVariableErrorMessage = /x is too large/
+const infinityResultErrorMessage = /Result is too large/
+
 QUnit.module('Тестируем функцию solve', function () {
 	QUnit.test('solve работает правильно ', function (assert) {
 		assert.strictEqual(solve('4 + 8', 5), 12);
@@ -11,16 +17,29 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
 		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
 	});
-	QUnit.test('тест валидации', function (assert) {
-		assert.strictEqual(solve('alert("hello world")', 1), null);
-		assert.strictEqual(solve('“; drop table users', 5), null);
-		assert.strictEqual(solve('x / 1', 1), null);
-		assert.strictEqual(solve('x**2', 3), null);
-		assert.strictEqual(solve('()', 1), null);
-		assert.strictEqual(solve('x)', 1), null);
-		assert.strictEqual(solve('(x+1', 1), null);
-		assert.strictEqual(solve('*', 1), null);
-		assert.strictEqual(solve('4 + x', 'abd'), null);
-		assert.strictEqual(solve('4', 'abd'), null);
+	QUnit.test('тест валидации выражения', function (assert) {
+		assert.throws(() => solve('alert("hello world")', 1), formulaErrorMessage);
+		assert.throws(() => solve('“; drop table users', 5), formulaErrorMessage);
+		assert.throws(() => solve('x / 1', 1), formulaErrorMessage);
+		assert.throws(() => solve('x**2', 3), formulaErrorMessage);
+		assert.throws(() => solve('()', 1), defaultErrorMessage);
+		assert.throws(() => solve('x)', 1), defaultErrorMessage);
+		assert.throws(() => solve('(x+1', 1), defaultErrorMessage);
+		assert.throws(() => solve('*', 1), defaultErrorMessage);
+	});
+	QUnit.test('тест валидации параметра x', function (assert) {
+		assert.throws(() => solve('4 + x', 'abd'), variableErrorMessage);
+		assert.throws(() => solve('4', 'abd'), variableErrorMessage);
+	});
+	QUnit.test('поддержка передачи объектов', function (assert) {
+		assert.strictEqual(solve(new String('x * x').toString(), 3), 9);
+		assert.strictEqual(solve(new String('x * x'), 3), 9);
+		assert.strictEqual(solve('x', new Number(4).valueOf()), 4);
+		assert.strictEqual(solve('x', new Number(4)), 4);
+	});
+	QUnit.test('выход за диапазон чисел', function (assert) {
+		assert.throws(() => solve('x', 2 * 10 ** 308), infinityVariableErrorMessage);
+		assert.throws(() => solve('x * x', 2 * 10 ** 307), infinityResultErrorMessage);
+		assert.strictEqual(solve('x', Number.MIN_VALUE * 10 ** (-10)), 0);
 	});
 });
