fbd9c232d283cf3fb226fe892e059a970aa2df88
darleet <darleet@mail.ru>
2024-09-08 21:31:37 +0300

    minmax test: add tests on multiple values

--- tests/minmax.js
+++ tests/minmax.js
@@ -25,8 +25,11 @@ QUnit.module('Тестируем функцию minmax', function () {
 		assert.deepEqual(minmax('0 0 0 0'), [ 0, 0 ]);
 		assert.deepEqual(minmax('1 1 1 1'), [ 1, 1 ]);
 		assert.deepEqual(minmax('1 2 3 4'), [ 1, 4 ]);
+		assert.deepEqual(minmax('-1 -2 -3 -4'), [-4, -1])
 		assert.deepEqual(minmax('-Infinity -1 0 1 Infinity'), [ -Infinity, Infinity ]);
 		assert.deepEqual(minmax('-.01 0 .01'), [ -.01, .01 ]);
+		assert.deepEqual(minmax('Infinity Infinity'), [ Infinity, Infinity ]);
+		assert.deepEqual(minmax('-Infinity -Infinity'), [ -Infinity, -Infinity ]);
 	});
 
 	QUnit.test('minmax игнорирует обычный текст', function (assert) {
