6e294ed1e0cc5db57eab79bd8dcccc91388fe537
Andrey <111575820+andrey-918@users.noreply.github.com>
2024-09-08 12:38:08 +0300

    Add files via upload

--- tests/chess.js
+++ tests/chess.js
@@ -1,11 +1,14 @@
 'use strict';
 
 QUnit.module('Тестируем функцию chess', function () {
-	QUnit.test('Шахматная доска 1 на 1', function (assert) {
+	QUnit.test('Шахматной доски 1 на 1 не бывает', function (assert) {
 		const expected =
-			'*\n'
-			assert.strictEqual(chess(1), expected);
-			assert.strictEqual(chess('1'), expected);
+			'Size must be a positive integer more than 1.'
+		assert.throws(
+			() => chess(1), Error(expected), 'Не бывает доски 1 на 1');
+		assert.throws(
+			() => chess('1'), Error(expected), 'Не бывает доски 1 на 1');
+			
 	});
 
 	QUnit.test('Шахматная доска 2 на 2', function (assert) {
@@ -51,90 +54,91 @@ QUnit.module('Тестируем функцию chess', function () {
 
 	QUnit.test('Шахматная доска 0 на 0', function (assert) {
 		assert.throws(
-			() => chess(0), Error('Size must be more than 1'), 'Не бывает доски 0 на 0');
+			() => chess(0), Error('Size must be a positive integer more than 1.'), 'Не бывает доски 0 на 0');
 		assert.throws(
-			() => chess('0'), Error('Size must be more than 1'), 'Не бывает доски 0 на 0');
+			() => chess('0'), Error('Size must be a positive integer more than 1.'), 'Не бывает доски 0 на 0');
 	});
 
 	QUnit.test('Шахматная доска -10 на -10', function (assert) {
 		assert.throws(
-			() => chess(-10), Error('Size must be more than 1'), 'Не бывает доски -10 на -10');
+			() => chess(-10), Error('Size must be a positive integer more than 1.'), 'Не бывает доски -10 на -10');
 		assert.throws(
-			() => chess('-10'), Error('Size must be more than 1'), 'Не бывает доски -10 на -10');
+			() => chess('-10'), Error('Size must be a positive integer more than 1.'), 'Не бывает доски -10 на -10');
 	});
 
 	QUnit.test('Шахматная доска размером не int', function (assert) {
-		const expected = 'Size must be an integer.';
+		const expected = 'Size must be a number.';
 
-		assert.throws(
-			() => chess(9.2), 
-			new Error(expected), 
-			'Должна быть ошибка для нецелого числа'
-		);
 
 		assert.throws(
 			() => chess('9.2'), 
-			new Error(expected), 
-			'Должна быть ошибка для строки с числом'
+			new Error('Size must be an integer.'), 
+			'Должна быть ошибка для строки с нецелым числом'
 		);
 
 		assert.throws(
-			() => chess('nothing'), 
-			new Error(expected), 
+			() => chess('-9.2'), 
+			new Error('Size must be an integer.'), 
+			'Должна быть ошибка для строки с нецелым числом'
+		);
+
+		assert.throws(
+			() => chess('not1hing'), 
+			new Error('Size must be an integer.'), 
 			'Должна быть ошибка для строки без числа'
 		);
 
 		assert.throws(
 			() => chess('something'), 
-			new Error(expected), 
+			new Error('Size must be an integer.'), 
 			'Должна быть ошибка для строки с текстом'
 		);
 
 		assert.throws(
 			() => chess([]), 
-			new Error(expected), 
+			new TypeError(expected), 
 			'Должна быть ошибка для пустого массива'
 		);
 
 		assert.throws(
 			() => chess([1, 2, 911]), 
-			new Error(expected), 
+			new TypeError(expected), 
 			'Должна быть ошибка для массива с числами'
 		);
 
 		assert.throws(
 			() => chess({}), 
-			new Error(expected), 
+			new TypeError(expected), 
 			'Должна быть ошибка для пустого объекта'
 		);
 
 		assert.throws(
 			() => chess({1: 8}), 
-			new Error(expected), 
+			new TypeError(expected), 
 			'Должна быть ошибка для объекта с числовым ключом'
 		);
 
 		assert.throws(
 			() => chess({'1': 8}), 
-			new Error(expected), 
+			new TypeError(expected), 
 			'Должна быть ошибка для объекта со строковым ключом'
 		);
 
 		assert.throws(
 			() => chess(), 
-			new Error(expected), 
+			new TypeError(expected), 
 			'Должна быть ошибка для отсутствующего аргумента'
 		);
 
 		assert.throws(
 			() => chess(null), 
-			new Error(expected), 
+			new TypeError(expected), 
 			'Должна быть ошибка для null'
 		);
 
 		assert.throws(
 			() => chess(true), 
-			new Error(expected), 
+			new TypeError(expected), 
 			'Должна быть ошибка для булевого значения'
 		);
 
