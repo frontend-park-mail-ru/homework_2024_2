f723430d89cb73b733de166f26d7ef055eb497df
darleet <darleet@mail.ru>
2024-09-08 22:50:36 +0300

    minmax ref: linter fix

--- source/minmax.js
+++ source/minmax.js
@@ -7,13 +7,13 @@
  * @throws {TypeError} Если входная строка не является строкой.
  */
 const minmax = function (text) {
-    if (typeof text !== 'string') {
-        throw new TypeError('Функция принимает на вход только тип string')
-    }
-    const numbers = text
-        .split(/ +/)
-        .map(Number.parseFloat)
-        .filter(entry => !isNaN(entry));
-    if (!numbers.length) return [undefined, undefined];
-    return [Math.min(...numbers), Math.max(...numbers)];
+	if (typeof text !== 'string') {
+		throw new TypeError('Функция принимает на вход только тип string')
+	}
+	const numbers = text
+		.split(/ +/)
+		.map(Number.parseFloat)
+		.filter(entry => !isNaN(entry));
+	if (!numbers.length) return [undefined, undefined];
+	return [Math.min(...numbers), Math.max(...numbers)];
 };
