e701a16b4ae3d48f07407f70f58dd2338de0e834
Andrey <111575820+andrey-918@users.noreply.github.com>
2024-09-08 11:05:50 +0300

    Add files via upload

--- tests/chess.js
+++ tests/chess.js
@@ -1,9 +1,11 @@
 'use strict';
 
 QUnit.module('Тестируем функцию chess', function () {
-	QUnit.test('Шахматной доски 1 на 1 не бывает', function (assert) {
-		assert.strictEqual(chess(1), null);
-		assert.strictEqual(chess('1'), null);
+	QUnit.test('Шахматная доска 1 на 1', function (assert) {
+		const expected =
+			'*\n'
+			assert.strictEqual(chess(1), expected);
+			assert.strictEqual(chess('1'), expected);
 	});
 
 	QUnit.test('Шахматная доска 2 на 2', function (assert) {
@@ -23,16 +25,6 @@ QUnit.module('Тестируем функцию chess', function () {
 		assert.strictEqual(chess('3'), expected);
 	});
 
-	QUnit.test('Шахматная доска 6 на 6', function (assert) {
-		const expected =
-			'* * \n' +
-			' * *\n' +
-			'* * \n' +
-			' * *\n';
-		assert.strictEqual(chess(6), expected);
-		assert.strictEqual(chess('6'), expected);
-	});
-
 	QUnit.test('Шахматная доска 8 на 8', function (assert) {
 		const expected =
 			'* * * * \n' +
@@ -47,18 +39,106 @@ QUnit.module('Тестируем функцию chess', function () {
 		assert.strictEqual(chess('8'), expected);
 	});
 
-	QUnit.test('Шахматная доска 0 на 0', function (assert) {
+	QUnit.test('Шахматная доска 4 на 4', function (assert) {
 		const expected =
-			null;
-		assert.strictEqual(chess(0), expected);
-		assert.strictEqual(chess('0'), expected);
+			'* * \n' +
+			' * *\n' +
+			'* * \n' +
+			' * *\n';
+		assert.strictEqual(chess(4), expected);
+		assert.strictEqual(chess('4'), expected);
+	});
+
+	QUnit.test('Шахматная доска 0 на 0', function (assert) {
+		assert.throws(
+			() => chess(0), Error('Size must be more than 1'), 'Не бывает доски 0 на 0');
+		assert.throws(
+			() => chess('0'), Error('Size must be more than 1'), 'Не бывает доски 0 на 0');
 	});
 
 	QUnit.test('Шахматная доска -10 на -10', function (assert) {
-		const expected =
-			null;
-		assert.strictEqual(chess(-10), expected);
-		assert.strictEqual(chess('-10'), expected);
+		assert.throws(
+			() => chess(-10), Error('Size must be more than 1'), 'Не бывает доски -10 на -10');
+		assert.throws(
+			() => chess('-10'), Error('Size must be more than 1'), 'Не бывает доски -10 на -10');
+	});
+
+	QUnit.test('Шахматная доска размером не int', function (assert) {
+		const expected = 'Size must be an integer.';
+
+		assert.throws(
+			() => chess(9.2), 
+			new Error(expected), 
+			'Должна быть ошибка для нецелого числа'
+		);
+
+		assert.throws(
+			() => chess('9.2'), 
+			new Error(expected), 
+			'Должна быть ошибка для строки с числом'
+		);
+
+		assert.throws(
+			() => chess('nothing'), 
+			new Error(expected), 
+			'Должна быть ошибка для строки без числа'
+		);
+
+		assert.throws(
+			() => chess('something'), 
+			new Error(expected), 
+			'Должна быть ошибка для строки с текстом'
+		);
+
+		assert.throws(
+			() => chess([]), 
+			new Error(expected), 
+			'Должна быть ошибка для пустого массива'
+		);
+
+		assert.throws(
+			() => chess([1, 2, 911]), 
+			new Error(expected), 
+			'Должна быть ошибка для массива с числами'
+		);
+
+		assert.throws(
+			() => chess({}), 
+			new Error(expected), 
+			'Должна быть ошибка для пустого объекта'
+		);
+
+		assert.throws(
+			() => chess({1: 8}), 
+			new Error(expected), 
+			'Должна быть ошибка для объекта с числовым ключом'
+		);
+
+		assert.throws(
+			() => chess({'1': 8}), 
+			new Error(expected), 
+			'Должна быть ошибка для объекта со строковым ключом'
+		);
+
+		assert.throws(
+			() => chess(), 
+			new Error(expected), 
+			'Должна быть ошибка для отсутствующего аргумента'
+		);
+
+		assert.throws(
+			() => chess(null), 
+			new Error(expected), 
+			'Должна быть ошибка для null'
+		);
+
+		assert.throws(
+			() => chess(true), 
+			new Error(expected), 
+			'Должна быть ошибка для булевого значения'
+		);
+
+	
 	});
 
 });
