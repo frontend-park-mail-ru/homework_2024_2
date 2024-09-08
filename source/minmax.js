522b124ebdb3c19434207c48a00f55a589726e08
darleet <darleet@mail.ru>
2024-09-08 22:02:42 +0300

    minmax ref: add docstring for func

--- source/minmax.js
+++ source/minmax.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * Находит минимальное и максимальное числа в строке.
+ * @param {string} text
+ * @returns {[number,number]|[undefined,undefined]}
+ * @throws {TypeError} Если входная строка не является строкой.
+ */
 const minmax = function (text) {
     if (typeof text !== 'string') {
         throw new TypeError('Функция принимает на вход только тип string')
