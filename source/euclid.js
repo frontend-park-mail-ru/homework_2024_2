b23970f792707a968bed72e05e458e2ec86a0e22
Fedoseev Vladimir <oruchim@yandex.ru>
2024-09-09 05:10:04 +0300

    реализована функция которая позволяет находить НОД натуральных чисел. Функция должна работать с любым количеством аргументов

---
+++ source/euclid.js
@@ -0,0 +1,15 @@
+
+const gcd = (solution, n) => {
+    while (n != 0) { 
+        [solution, n] = [n, solution % n]; //solution = n , n = solution % n
+    }
+    return Math.abs(solution);
+};
+
+const euclid = (...inputNumbers) => {
+    if (!inputNumbers.every(n => Number.isInteger(n))) {
+        return 'Все аргументы должны быть целыми числами'; // Обработка некорректного ввода
+    }
+    if (inputNumbers.length == 1) return inputNumbers[0]; // Если введено одно число, то его НОД это оно само и есть!
+    return inputNumbers.reduce((acc, n) => gcd(acc, n));
+}; 
