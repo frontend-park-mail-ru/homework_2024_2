3d1ec3862bfb28b7b5240b1e1d90b46436fb8616
qwerty_268 <lev-demchenko@bk.ru>
2024-09-09 14:25:23 +0300

    fix: JSDoc

--- source/zip.js
+++ source/zip.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/**
+    * функция выделяющая все параметры и их значения
+    * @param {obj} ...objects - Объекты, из которых выделяем значения и параметры
+    * @returns {obj} Объект, содержащий все поля и значения объектов, переданных в качестве параметров
+    * @throws {Error} Если нет параметров
+    * @throws {TypeError} Если параметр nil
+ */
 const zip = (...objects) => {
     if (!objects.length) {
          throw new Error('No parameters');
@@ -13,8 +20,6 @@ const zip = (...objects) => {
         }
 
         for (const [key, value] of Object.entries(obj)) {
-
-
             if (!result.hasOwnProperty(key)) {
                     result[key] = value;
             }
