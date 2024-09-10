ef55cf1d09962220a4c8f622a1e8bb3ebeeebed0
Ekaterina Ambartsumova <ekaterina.amba@mail.ru>
2024-09-10 15:56:09 +0300

    modified: removed additional ' '  source/set.js

--- source/set.js
+++ source/set.js
@@ -5,21 +5,21 @@
  * Recursively inserts newProperty to object by the path
  * @param {Object} object - object to change
  * @param {String[]} propertyList - array of strings 
- * @param {Number|String|BigInt|Boolean|Object|null|undefined|symbol|
- *          |Number[]|String[]|BigInt[]|Boolean[]|Object[]} newProperty - instance of any type: number, string, etc.                                     
+ * @param {Number|String|BigInt|Boolean|Object|null|undefined|Symbol|
+ *          |Number[]|String[]|BigInt[]|Boolean[]|Object[]|Symbol[]} newProperty - instance of any type: number, string, etc.                                     
  * @returns {Object} Returns modified object
  */
-const changeNestedProperty = ( object, propertyList, newProperty ) =>
+const changeNestedProperty = (object, propertyList, newProperty) =>
     {
-            if ( !propertyList.length )
+            if (!propertyList.length)
             {
                 return newProperty;
             }
-            if ( !object.hasOwnProperty( propertyList[ 0 ] ) )
+            if (!object.hasOwnProperty(propertyList[0]))
             {
-                object[ propertyList[ 0 ] ] = {};
+                object[propertyList[0]] = {};
             }
-            object[ propertyList[ 0 ] ] = changeNestedProperty( object[ propertyList[ 0 ] ], propertyList.slice( 1 ), newProperty );
+            object[propertyList[0]] = changeNestedProperty(object[propertyList[0]], propertyList.slice(1), newProperty);
             return object;
             
     };
@@ -29,18 +29,17 @@ const changeNestedProperty = ( object, propertyList, newProperty ) =>
  * Inserts newProperty to object by the path
  * @param {Object} object - object to change
  * @param {String} path - string, which describes path to nested property newProperty: each path element must contain "." before itself
- * @param {Number|String|BigInt|Boolean|Object|null|undefined
-*          |Number[]|String[]|BigInt[]|Boolean[]|Object[]} newProperty - instance of any type: number, string, etc.                                     
+ * @param {Number|String|BigInt|Boolean|Object|null|undefined|Symbol|
+ *          |Number[]|String[]|BigInt[]|Boolean[]|Object[]|Symbol[]} newProperty - instance of any type: number, string, etc.                                     
 * @returns {Object} Returns modified object if path is correct else returns original object
 */
-const set = ( object, path, newProperty ) =>
+const set = (object, path, newProperty) =>
 {
-    if ( typeof path !== "string" || ! path.length || path[ 0 ] !== "." )
+    if (typeof path !== "string" || !path.length || path[0] !== ".")
     {
         return object;
     }
-    let propertyList = path.split( "." ).slice( 1 );
-    let newObject = changeNestedProperty( object, propertyList, newProperty );
-    
+    let propertyList = path.split(".").slice(1);
+    let newObject = changeNestedProperty(object, propertyList, newProperty);
     return newObject;
 };
