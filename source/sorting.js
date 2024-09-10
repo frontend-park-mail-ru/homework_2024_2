46563f3c40f1cd6d62b01bd70543e4db3c457d77
Valentin <v_stremin@mail.ru>
2024-09-10 21:05:40 +0300

    Поправки по стилю коду

--- source/sorting.js
+++ source/sorting.js
@@ -14,67 +14,67 @@
  */
 
 const sorting = (arr, props) => {
-  if (!arr || !props) {
-    throw new TypeError('Either arr or props is undefined');
-  }
+	if (!arr || !props) {
+		throw new TypeError('Either arr or props is undefined');
+	}
 
-  if (!Array.isArray(arr)) {
-    throw new TypeError('arr should be array');
-  }
+	if (!Array.isArray(arr)) {
+		throw new TypeError('arr should be array');
+	}
 
-  if (!Array.isArray(props)) {
-    throw new TypeError('props should be array');
-  }
+	if (!Array.isArray(props)) {
+		throw new TypeError('props should be array');
+	}
 
-  arr.forEach((elem) => {
-    if (typeof elem !== 'object' || elem === null) {
-      throw new TypeError(
-        `arr should contain objects only, not ${typeof elem}`
-      );
-    }
+	arr.forEach((elem) => {
+		if (typeof elem !== 'object' || elem === null) {
+		throw new TypeError(
+			`arr should contain objects only, not ${typeof elem}`
+		);
+		}
 
-    props.forEach((prop) => {
-      if (elem[prop] === undefined) {
-        throw new Error(
-          `At least one plain object in arr doesn't contain orderby prop ${prop}`
-        );
-      }
-    });
-  });
+		props.forEach((prop) => {
+		if (elem[prop] === undefined) {
+			throw new Error(
+			`At least one plain object in arr doesn't contain orderby prop ${prop}`
+			);
+		}
+		});
+	});
 
-  if (arr.length <= 1) {
-    return arr;
-  }
+	if (arr.length <= 1) {
+		return arr;
+	}
 
-  const sortedArr = [...arr];
+	const sortedArr = [ ...arr ];
 
-  sortedArr.forEach((_, i) => {
-    let swapped = false;
+	sortedArr.forEach((_, i) => {
+		let swapped = false;
 
-    sortedArr.forEach((_, j) => {
-      if (j < sortedArr.length - 1 - i) {
-        let flag = false;
+		sortedArr.forEach((_, j) => {
+		if (j < sortedArr.length - 1 - i) {
+			let flag = false;
 
-        for (let prop of props) {
-          if (sortedArr[j][prop] > sortedArr[j + 1][prop]) {
-            flag = true;
-            break;
-          } else if (sortedArr[j][prop] < sortedArr[j + 1][prop]) {
-            break;
-          }
-        }
+			for (let prop of props) {
+				if (sortedArr[j][prop] > sortedArr[j + 1][prop]) {
+					flag = true;
+					break;
+				} else if (sortedArr[j][prop] < sortedArr[j + 1][prop]) {
+					break;
+				}
+			}
 
-        if (flag) {
-          [sortedArr[j], sortedArr[j + 1]] = [sortedArr[j + 1], sortedArr[j]];
-          swapped = true;
-        }
-      }
-    });
+			if (flag) {
+				[sortedArr[j], sortedArr[j + 1]] = [sortedArr[j + 1], sortedArr[j]];
+				swapped = true;
+			}
+		}
+		});
 
-    if (!swapped) {
-      return sortedArr;
-    }
-  });
+		if (!swapped) {
+			return sortedArr;
+		}
+	});
 
-  return sortedArr;
+	return sortedArr;
 };
