df01e32b99c1050dcefd79fb2f845cdaf63c4715
Ekaterina Ambartsumova <ekaterina.amba@mail.ru>
2024-09-10 14:55:44 +0300

    modified source/set.js: add jsdoc

--- source/set.js
+++ source/set.js
@@ -1,31 +1,43 @@
-'use strict';
+"use strict";
 
 
 /**
  * Recursively inserts newProperty to object by the path
+ * @param {Object} object - object to change
+ * @param {String[]} propertyList - array of strings 
+ * @param {Number|String|BigInt|Boolean|Object|null|undefined
+ *          |Number[]|String[]|BigInt[]|Boolean[]|Object[]} newProperty - instance of any type: number, string, etc.                                     
  * @returns {Object} Returns modified object
  */
-const changeNestedProperty = (object, propertyList, newProperty) =>
+const changeNestedProperty = ( object, propertyList, newProperty ) =>
     {
-            if ( !( propertyList.length ) )
+            if ( !propertyList.length )
             {
                 return newProperty;
             }
 
-            if ( !( propertyList[0] in object ) )
+            if ( !object.hasOwnProperty( propertyList[ 0 ] ) )
             {
-                object[ propertyList[0] ] = {};
+                object[ propertyList[ 0 ] ] = {};
             }
  
-            object[ propertyList[0] ] = changeNestedProperty( object[propertyList[0] ], propertyList.slice(1), newProperty ); ;
+            object[ propertyList[ 0 ] ] = changeNestedProperty( object[propertyList[ 0 ] ], propertyList.slice( 1 ), newProperty );
             return object;
             
     };
 
 
-const set = (object, path, newProperty) =>
+/**
+ * Inserts newProperty to object by the path
+ * @param {Object} object - object to change
+ * @param {String} path - string, which describes path to nested property newProperty
+ * @param {Number|String|BigInt|Boolean|Object|null|undefined
+*          |Number[]|String[]|BigInt[]|Boolean[]|Object[]} newProperty - instance of any type: number, string, etc.                                     
+* @returns {Object} Returns modified object if path is correct else returns original object
+*/
+const set = ( object, path, newProperty ) =>
 {
-    if ( path[0] !== "." )
+    if ( typeof path !== "string" || path[ 0 ] !== "." || ! path.length)
     {
         return object;
     }
