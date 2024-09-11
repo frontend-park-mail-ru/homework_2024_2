08a6792ffaa45b9b757df4d9c0c53bb6d2d017f2
Fedoseev Vladimir <oruchim@yandex.ru>
2024-09-11 22:11:07 +0300

    Исправил код в соответствии с замечаниями

--- source/euclid.js
+++ source/euclid.js
@@ -1,16 +1,27 @@
 'use strict';
 
 const gcd = (solution, n) => {
-    while (n !== 0) { 
-        [solution, n] = [n, solution % n]; //solution = n , n = solution % n
+    while (n) {
+        const temp = solution;
+        solution = n;
+        n = temp % n;
     }
     return Math.abs(solution);
 };
 
 const euclid = (...inputNumbers) => {
-    if (!inputNumbers.every(n => Number.isInteger(n))) {
-        throw new TypeError('Все аргументы должны быть целыми числами'); // Обработка некорректного ввода
+
+    inputNumbers = inputNumbers.flat(Infinity); //если массив содержит другие массивы в качестве своих элементов, то метод flat() позволяет уменьшить вложенность, вплоть до полного превращения массива в плоский
+    if (!inputNumbers.length) {
+        return 'Необходимо передать хотя бы одно число';
     }
-    if (inputNumbers.length === 1) return inputNumbers[0]; // Если введено одно число, то его НОД это оно само и есть!
+    inputNumbers = inputNumbers.map(n => Number(n)); // преобразовывет элементы в числа
+
+    if (!inputNumbers.every(n => Number.isInteger(n))) {
+        throw new TypeError('Все аргументы должны быть целыми числами');
+    }
+
+    if (inputNumbers.length === 1) return inputNumbers[0];
+
     return inputNumbers.reduce((acc, n) => gcd(acc, n));
-}; 
+};
