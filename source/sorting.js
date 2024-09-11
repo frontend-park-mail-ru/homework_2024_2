16768782411949f9f52cf6d248655e597967873f
Valentin <v_stremin@mail.ru>
2024-09-11 16:24:05 +0300

    Декомпозировал функцию sorting

--- source/sorting.js
+++ source/sorting.js
@@ -1,19 +1,17 @@
 'use strict';
 
 /**
- * Принимает на вход массив plain-объектов и массив свойств, по которым необходимо отсортировать массив объектов. Реализует устойчивую сортировку этого массива в порядке возрастания.
- *
+ *  Валидирует входные данные
+ * 
  * @param {object[]} arr массив plain-объектов
  * @param {string[]} props массив свойств, по которым необходимо отсортировать массив
- * @returns {object[]} Отсортированный массив
  * @throws {TypeError} Выдаст ошибку, если один из аргументов не передан
  * @throws {TypeError} Выдаст ошибку, если arr это не массив
  * @throws {TypeError} Выдаст ошибку, если props это не массив
  * @throws {TypeError} Выдаст ошибку, если хотя бы один элемент arr не является объектом
  * @throws {Error} Выдаст ошибку, если хотя бы один объект не содержит свойство, по которому arr сортируется
  */
-
-const sorting = (arr, props) => {
+const validations = (arr, props) => {
 	if (!arr || !props) {
 		throw new TypeError('Either arr or props is undefined');
 	}
@@ -41,7 +39,35 @@ const sorting = (arr, props) => {
 		}
 		});
 	});
+}
 
+/**
+ * Сравнивает два объекта по переданным свойствам
+ * 
+ * @param {any} a первый элемент 
+ * @param {any} b второй элемент
+ * @param {string[]} props 
+ * @returns {number} результат сравнения двух элементов
+ */
+const compareObjects = (a, b, props) => {
+	for (let prop of props) {
+		if (a[prop] > b[prop]) {
+			return 1;
+		} else if (a[prop] < b[prop]) {
+			return -1;
+		}
+	}
+	return 0;
+}
+
+/**
+ * Реализует сортировку plain-объектов методом пузырька
+ * 
+ * @param {object[]} arr массив plain-объектов
+ * @param {string[]} props массив свойств, по которым необходимо отсортировать массив
+ * @returns {object[]} отсортированный массив
+ */
+const bubbleSort = (arr, props) => {
 	if (arr.length <= 1) {
 		return arr;
 	}
@@ -52,24 +78,11 @@ const sorting = (arr, props) => {
 		let swapped = false;
 
 		sortedArr.forEach((_, j) => {
-			if (j < sortedArr.length - 1 - i) {
-				let flag = false;
-
-				for (let prop of props) {
-					if (sortedArr[j][prop] > sortedArr[j + 1][prop]) {
-						flag = true;
-						break;
-					} else if (sortedArr[j][prop] < sortedArr[j + 1][prop]) {
-						break;
-					}
-				}
-
-				if (flag) {
-					[sortedArr[j], sortedArr[j + 1]] = [sortedArr[j + 1], sortedArr[j]];
-					swapped = true;
-				}
+			if ((j < sortedArr.length - 1 - i) && (compareObjects(sortedArr[j], sortedArr[j + 1], props) > 0)) {
+				[sortedArr[j], sortedArr[j + 1]] = [sortedArr[j + 1], sortedArr[j]];
+				swapped = true;
 			}
-		});
+		})
 
 		if (!swapped) {
 			return sortedArr;
@@ -77,4 +90,19 @@ const sorting = (arr, props) => {
 	});
 
 	return sortedArr;
-};
+}
+
+
+/**
+ * Принимает на вход массив plain-объектов и массив свойств, по которым необходимо отсортировать массив объектов.
+ * Реализует устойчивую сортировку этого массива в порядке возрастания.
+ * 
+ * @param {object[]} arr массив plain-объектов
+ * @param {string[]} props массив свойств, по которым необходимо отсортировать массив
+ * @returns {object[]} отсортированный массив
+ */
+const sorting = (arr, props) => {
+	validations(arr, props);
+
+	return bubbleSort(arr, props);
+}
