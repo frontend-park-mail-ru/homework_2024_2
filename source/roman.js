850b5b06b7e74e9c0bbf22e265d8c331886de695
Tatiana Bocharova <tanka.bocharova.23@gmail.com>
2024-09-11 11:35:11 +0300

    code style and objects as params

--- source/roman.js
+++ source/roman.js
@@ -1,19 +1,19 @@
 'use strict';
 
 const arabicRomanMap = {
-        'M': 1000,
-        'CM': 900,
-        'D': 500,
-        'CD': 400,
-        'C': 100,
-        'XC': 90,
-        'L': 50,
-        'XL': 40,
-        'X': 10,
-        'IX': 9,
-        'V': 5,
-        'IV': 4,
-        'I': 1
+	'M': 1000,
+	'CM': 900,
+	'D': 500,
+	'CD': 400,
+	'C': 100,
+	'XC': 90,
+	'L': 50,
+	'XL': 40,
+	'X': 10,
+	'IX': 9,
+	'V': 5,
+	'IV': 4,
+	'I': 1
 };
 
 const ROMAN_TYPE_TEST_REGEX = /^[ivxlcdm]+$/i;
@@ -26,20 +26,21 @@ const ROMAN_TYPE_TEST_REGEX = /^[ivxlcdm]+$/i;
  * @throws {TypeError} Throws an error if the input data is of the wrong type or format.
  */
 const roman = (numberOrNumeral) => {
-        if (typeof numberOrNumeral === 'number' ||
-                typeof numberOrNumeral === 'string' ||
-                numberOrNumeral instanceof String) {
-                if (Number.isInteger(+numberOrNumeral) && +numberOrNumeral > 0) {
-                        return arabicToRoman(+numberOrNumeral);
-                }
-                if (typeof numberOrNumeral === 'string'|| numberOrNumeral instanceof String) {
-                        if (!ROMAN_TYPE_TEST_REGEX.test(numberOrNumeral)) {
-                                throw new TypeError('Неверный формат римского числа');
-                        }
-                        return romanToArabic(numberOrNumeral);
-                }
-        }
-        throw new TypeError('Неверный тип данных');
+	if (typeof numberOrNumeral === 'number' ||
+		typeof numberOrNumeral === 'string' ||
+		numberOrNumeral instanceof Number ||
+		numberOrNumeral instanceof String) {
+		if (Number.isInteger(+numberOrNumeral) && +numberOrNumeral > 0) {
+			return arabicToRoman(+numberOrNumeral);
+		}
+		if (typeof numberOrNumeral === 'string' || numberOrNumeral instanceof String) {
+			if (!ROMAN_TYPE_TEST_REGEX.test(numberOrNumeral)) {
+				throw new TypeError('Неверный формат римского числа');
+			}
+			return romanToArabic(numberOrNumeral);
+		}
+	}
+	throw new TypeError('Неверный тип данных');
 }
 
 /**
@@ -50,16 +51,16 @@ const roman = (numberOrNumeral) => {
  * @throws {TypeError} Throws an error if Roman numeral is of the wrong format.
  */
 const romanToArabic = (romanNumeral) => {
-        const upperCaseRomanNumeral = romanNumeral.toUpperCase();
-        return [...upperCaseRomanNumeral].reduce((result, currentSymbol, i) => {
-                const nextSymbol = upperCaseRomanNumeral[i + 1];
-                if (arabicRomanMap[currentSymbol] < arabicRomanMap[nextSymbol]) {
-                        result -= arabicRomanMap[currentSymbol];
-                } else {
-                        result += arabicRomanMap[currentSymbol];
-                }
-                return result;
-        }, 0);
+	const upperCaseRomanNumeral = romanNumeral.toUpperCase();
+	return [...upperCaseRomanNumeral].reduce((result, currentSymbol, i) => {
+		const nextSymbol = upperCaseRomanNumeral[i + 1];
+		if (arabicRomanMap[currentSymbol] < arabicRomanMap[nextSymbol]) {
+			result -= arabicRomanMap[currentSymbol];
+		} else {
+			result += arabicRomanMap[currentSymbol];
+		}
+		return result;
+	}, 0);
 }
 
 /**
@@ -69,14 +70,14 @@ const romanToArabic = (romanNumeral) => {
  * @return {string|number} - converted Roman numeral
  */
 const arabicToRoman = (arabicNumber) => {
-        let copiedArabicNumber = arabicNumber;
-        return Object.entries(arabicRomanMap).reduce(
-                (result, [romanKey, arabicValue]) => {
-                        while (copiedArabicNumber >= arabicValue) {
-                                result += romanKey;
-                                copiedArabicNumber -= arabicValue;
-                        }
-                        return result;
-                }, '');
+	let copiedArabicNumber = arabicNumber;
+	return Object.entries(arabicRomanMap).reduce(
+		(result, [romanKey, arabicValue]) => {
+			while (copiedArabicNumber >= arabicValue) {
+				result += romanKey;
+				copiedArabicNumber -= arabicValue;
+			}
+			return result;
+		}, '');
 }
 
--- tests/roman.js
+++ tests/roman.js
@@ -1,68 +1,69 @@
 'use strict';
 
 QUnit.module('Тестируем функцию roman', function () {
-        QUnit.test('roman правильно переводит из римской системы счисления', function (assert) {
-                assert.strictEqual(roman('I'), 1);
-                assert.strictEqual(roman('V'), 5);
-                assert.strictEqual(roman('M'), 1000);
-                assert.strictEqual(roman('l'), 50);
-                assert.strictEqual(roman('d'), 500);
+	QUnit.test('roman правильно переводит из римской системы счисления', function (assert) {
+		assert.strictEqual(roman('I'), 1);
+		assert.strictEqual(roman('V'), 5);
+		assert.strictEqual(roman('M'), 1000);
+		assert.strictEqual(roman('l'), 50);
+		assert.strictEqual(roman('d'), 500);
 
-                assert.strictEqual(roman('iv'), 4);
-                assert.strictEqual(roman('iiii'), 4);
-                assert.strictEqual(roman('CM'), 900);
+		assert.strictEqual(roman('iv'), 4);
+		assert.strictEqual(roman('iiii'), 4);
+		assert.strictEqual(roman('CM'), 900);
 
-                assert.strictEqual(roman('MCMIV'), 1904);
-                assert.strictEqual(roman('MCMXC'), 1990);
-                assert.strictEqual(roman('mmxvii'), 2017);
-        });
+		assert.strictEqual(roman('MCMIV'), 1904);
+		assert.strictEqual(roman('MCMXC'), 1990);
+		assert.strictEqual(roman('mmxvii'), 2017);
+	});
 
-        QUnit.test('roman правильно переводит из десятичной системы счисления', function (assert) {
-                assert.strictEqual(roman(1), 'I');
-                assert.strictEqual(roman(5), 'V');
-                assert.strictEqual(roman(1000), 'M');
-                assert.strictEqual(roman(50), 'L');
-                assert.strictEqual(roman(500), 'D');
+	QUnit.test('roman правильно переводит из десятичной системы счисления', function (assert) {
+		assert.strictEqual(roman(1), 'I');
+		assert.strictEqual(roman(5), 'V');
+		assert.strictEqual(roman(1000), 'M');
+		assert.strictEqual(roman(50), 'L');
+		assert.strictEqual(roman(500), 'D');
 
-                assert.strictEqual(roman(4), 'IV');
-                assert.strictEqual(roman(900), 'CM');
+		assert.strictEqual(roman(4), 'IV');
+		assert.strictEqual(roman(900), 'CM');
 
-                assert.strictEqual(roman(1904), 'MCMIV');
-                assert.strictEqual(roman(1990), 'MCMXC');
-                assert.strictEqual(roman(2017), 'MMXVII');
-        });
+		assert.strictEqual(roman(1904), 'MCMIV');
+		assert.strictEqual(roman(1990), 'MCMXC');
+		assert.strictEqual(roman(2017), 'MMXVII');
+	});
 
-        QUnit.test('roman правильно определяет, что было передано на вход', function (assert) {
-                assert.strictEqual(roman('1904'), 'MCMIV');
-                assert.strictEqual(roman('1990'), 'MCMXC');
-                assert.strictEqual(roman('2017'), 'MMXVII');
-        });
+	QUnit.test('roman правильно определяет, что было передано на вход', function (assert) {
+		assert.strictEqual(roman('1904'), 'MCMIV');
+		assert.strictEqual(roman('1990'), 'MCMXC');
+		assert.strictEqual(roman('2017'), 'MMXVII');
+	});
 
-        QUnit.test('roman выдает ошибку при передаче невалидного типа данных', function (assert) {
-                assert.throws(function () {
-                        roman(NaN);
-                }, new TypeError('Неверный тип данных'));
-                assert.throws(function () {
-                        roman(1.23);
-                }, new TypeError('Неверный тип данных'));
-                assert.throws(function () {
-                        roman(-123);
-                }, new TypeError('Неверный тип данных'));
-                assert.throws(function () {
-                        roman(true);
-                }, new TypeError('Неверный тип данных'));
-        });
+	QUnit.test('roman выдает ошибку при передаче невалидного типа данных', function (assert) {
+		assert.throws(function () {
+			roman(NaN);
+		}, new TypeError('Неверный тип данных'));
+		assert.throws(function () {
+			roman(1.23);
+		}, new TypeError('Неверный тип данных'));
+		assert.throws(function () {
+			roman(-123);
+		}, new TypeError('Неверный тип данных'));
+		assert.throws(function () {
+			roman(true);
+		}, new TypeError('Неверный тип данных'));
+	});
 
-        QUnit.test('roman выдает ошибку при передаче несуществующего римского числа', function (assert) {
-                assert.throws(function () {
-                        roman('XYPR');
-                }, new TypeError('Неверный формат римского числа'));
-                assert.throws(function () {
-                        roman('kekw');
-                }, new TypeError('Неверный формат римского числа'));
-        });
+	QUnit.test('roman выдает ошибку при передаче несуществующего римского числа', function (assert) {
+		assert.throws(function () {
+			roman('XYPR');
+		}, new TypeError('Неверный формат римского числа'));
+		assert.throws(function () {
+			roman('kekw');
+		}, new TypeError('Неверный формат римского числа'));
+	});
 
-        QUnit.test('roman работает при передачe типа данных object', function (assert) {
-                assert.strictEqual(roman(new String('MCMIV')), 1904);
-        });
+	QUnit.test('roman работает при передачe типа данных object', function (assert) {
+		assert.strictEqual(roman(new String('MCMIV')), 1904);
+		assert.strictEqual(roman(new Number(1904)), 'MCMIV');
+	});
 });
