8dbd3066ba1079059ef592e9a4aacb9a8eae410b
qosmioo <79136983+qosmioo@users.noreply.github.com>
2024-09-12 14:30:22 +0300

    minor fixes.

--- source/format.js
+++ source/format.js
@@ -8,16 +8,19 @@ const format = (input, columns) => {
         throw new Error('Columns amount must be greater than array size');
     }
 
+    let widths = new Array(columns);
+    for (let i = 0; i < columns; i++) {
+        widths[i] = Math.max(...input.filter((_, j) => j % columns === i % columns)
+            .map(num => num.toString().length));
+    }
+
     let output = '';
     for (let i = 0; i < input.length; i++) {
         if (typeof input[i] !== 'number') {
             throw new TypeError('Input data must contain integers only')
         }
 
-        const currWidth = Math.max( ...input.filter( (_, j) => j % columns === i % columns)
-            .map( num => num.toString().length) );
-
-        output += input[i].toString().padStart(currWidth);
+        output += input[i].toString().padStart(widths[i % columns]);
         if (i === input.length - 1) {
             break;
         }
