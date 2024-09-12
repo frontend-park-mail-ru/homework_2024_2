e2a5eadfe3a31a59a11b6fadbd3eba8c2287a3e0
Alexander Novak <shura.novak.03@mail.ru>
2024-09-12 04:00:20 +0300

    realized solve without eval

--- source/solve.js
+++ source/solve.js
@@ -1,3 +1,74 @@
 'use strict';
 
-const solve = (str, xval) => eval(str.replaceAll('x', xval));
+
+function toPolish (str) {
+
+    let masStr = str.split("");
+
+    let priorities = new Map([
+        ['+', 1],
+        ['-', 1], 
+        ['*', 2]]);
+
+    let myStack = [];
+
+    let answer = "";
+
+    for (let val of masStr) {
+        if (val === '+' || val === '-' || val === '*') {
+            while (priorities.get(val) <= priorities.get(myStack.at(-1))){
+                answer += myStack.pop();
+            }
+            myStack.push(val);
+        }else if (val === '('){
+            myStack.push(val);
+        }else if (val === ')'){
+            while (myStack.at(-1) !== '('){
+                answer += myStack.pop();
+            }
+            myStack.pop();
+        }else if (val !== ' '){
+            answer += val;
+        }
+    }
+
+    while(myStack.length !== 0){
+        answer += myStack.pop();
+    }
+    
+    return answer;
+}
+
+function solvePolish(str) {
+
+    let myStack = [];
+
+    for (let elem of str) {
+        switch(elem) {
+            case '+':
+                myStack.push(myStack.pop() + myStack.pop());
+                break;
+            case '-':
+                let oper1 = myStack.pop();
+                let oper2 = myStack.pop();
+                myStack.push(oper2 - oper1);
+                break;
+            case '*':
+                myStack.push(myStack.pop() * myStack.pop());
+                break;
+            default:
+                myStack.push(Number(elem));
+                break;
+        }
+    }
+
+    return myStack.at(-1);
+
+}
+
+
+function solve (str, val) {
+    return solvePolish(toPolish(str.replaceAll('x', String(val))));
+}
+
+
