86f52e240778ee1aad154d611189a1c065bfd758
qosmioo <79136983+qosmioo@users.noreply.github.com>
2024-09-12 19:37:27 +0300

    changed loops.

--- source/format.js
+++ source/format.js
@@ -4,7 +4,7 @@
  * Форматирует массив чисел в строки, разбивая их на заданное количество столбцов.
  * Каждое число выравнивается по ширине, основанной на длине самого длинного числа в соответствующем столбце.
  *
- * @param {number[]} input - Массив целых чисел, которые нужно отформатировать.
+ * @param {number[]} input - Массив чисел, которые нужно отформатировать.
  * @param {number} columns - Количество столбцов для форматирования.
  * @returns {string} - Строка, представляющая отформатированные числа в столбцах.
  * @throws {RangeError} - Если количество столбцов меньше 1 или превышает размер массива.
@@ -18,29 +18,25 @@ const format = (input, columns) => {
         throw new RangeError('Columns amount must be greater than array size');
     }
 
-    let widths = new Array(columns);
-    for (let i = 0; i < columns; i++) {
+    let widths = [];
+    input.forEach((_, i) => {
         widths[i] = Math.max(...input.filter((_, j) => j % columns === i % columns)
             .map(num => num.toString().length));
-    }
+    })
 
     let output = '';
-    for (let i = 0; i < input.length; i++) {
+    input.forEach((_, i) => {
         if (typeof input[i] !== 'number') {
             throw new TypeError('Input data must contain integers only')
         }
 
         output += input[i].toString().padStart(widths[i % columns]);
-        if (i === input.length - 1) {
-            break;
-        }
-
-        if (i % columns === columns - 1) {
+        if (i % columns === columns - 1 && i !== input.length - 1) {
             output += '\n';
-        } else {
+        } else if (i !== input.length - 1) {
             output += ' ';
         }
-    }
+    })
 
     return output;
 }
