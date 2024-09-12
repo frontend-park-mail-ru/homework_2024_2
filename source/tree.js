e7860f17e2a41ba8a2a769d4f6463467a3e80e27
PtFux <polina.tikunowa@gmail.com>
2024-09-12 18:50:29 +0300

    added new types and reformat file

--- source/tree.js
+++ source/tree.js
@@ -3,7 +3,7 @@
 /**
   * Generates a tree-like string
   * @function
-  * @param {number | string} height - The high of tree.
+  * @param {number | string | String | Number} height - The high of tree.
   * @throws {TypeError} The parameter type is invalid.
   * @throws {Error} Parameter is not a number.
   * @throws {RangeError} The height must be greater than 3.
@@ -11,44 +11,45 @@
   */
 
 function tree(height) {
-    if ( !( (typeof height === 'number') || (typeof height == 'string')) ) {
-      throw new TypeError('The parameter type is invalid!');
-    }
-
-    if ( isNaN(height) ) {
-      throw new Error('Parameter is not a number!');
-    }
-
-    if (height < 3) {
-      throw new RangeError('The height must be greater than 3');
-    }
-    
-    let result = '';
-    let crown = '*';
-    let i = 0;
-
-    while (i < height - 1) { 
-      result += generateCrown(i, height);
-      crown += '**'
-      
-      i++;
-    }
-
-    const empty = ' '.repeat( (2*(height - 2)) / 2 );
-    result += `${empty}|${empty}\n`; 
-
-    return result;
+  if (!((typeof height === 'number') || (typeof height == 'string'))
+    || (height instanceof String) || (height instanceof Number)) {
+    throw new TypeError('The parameter type is invalid!');
   }
 
-  /**
-  * Generates a line in the tree crown
-  * @function
-  * @param {number | string} layer - The height of tree.
-  * @param {number | string} height - The сurrent crown level.
-  * @returns {string} The line in the tree crown.
-  */
+  if (isNaN(height)) {
+    throw new Error('Parameter is not a number!');
+  }
+
+  if (height < 3) {
+    throw new RangeError('The height must be greater than 3');
+  }
+
+  let result = '';
+  let crown = '*';
+  let i = 0;
+
+  while (i < height - 1) {
+    result += generateCrown(i, height);
+    crown += '**'
+
+    i++;
+  }
+
+  const empty = ' '.repeat((2 * (height - 2)) / 2);
+  result += `${empty}|${empty}\n`;
+
+  return result;
+}
+
+/**
+* Generates a line in the tree crown
+* @function
+* @param {number | string} layer - The height of tree.
+* @param {number | string} height - The сurrent crown level.
+* @returns {string} The line in the tree crown.
+*/
 
 function generateCrown(layer, height) {
-  const empty = " ".repeat( ( 2 * (height - 2) - layer * 2 ) / 2 );
+  const empty = " ".repeat((2 * (height - 2) - layer * 2) / 2);
   return `${empty}${"*".repeat(2 * layer + 1)}${empty}\n`
 }
