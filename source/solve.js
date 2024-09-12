57bc30949324e43e2d87dfdfe7fa4299c5b00281
Григорий Громыко <multiplaygames@mail.ru>
2024-09-12 17:32:23 +0300

    postfix notation v.2

--- source/solve.js
+++ source/solve.js
@@ -1,63 +1,73 @@
 'use strict';
 
 const infixToPostfix = (expression) => {
-  const precedence = { '+': 1, '-': 1, '*': 2 };
+  const precedence = { '+': 1, '-': 1, '*': 2, '/': 2 };
   const output = [];
   const operators = [];
+  let numberBuffer = '';
 
-  for (let char of expression) {
-    if (/\d/.test(char)) {
-      output.push(char);
-    }
-    else if (char === '(') {
-      operators.push(char);
-    }
-    else if (char === ')') {
-      while (operators.length && operators[operators.length - 1] !== '(') {
-        output.push(operators.pop());
+  for (let i = 0; i < expression.length; i++) {
+    const char = expression[i];
+
+    if (/\d/.test(char) || (char === '-' && (i === 0 || /[+\-*/(]/.test(expression[i - 1])))) {
+      numberBuffer += char;
+    } else {
+      if (numberBuffer) {
+        output.push(numberBuffer);
+        numberBuffer = '';
       }
-      operators.pop();
-    }
-    else if (['+', '-', '*'].includes(char)) {
-      while (operators.length && precedence[operators[operators.length - 1]] >= precedence[char]) {
-        output.push(operators.pop());
+
+      if (char === '(') {
+        operators.push(char);
+      } else if (char === ')') {
+        while (operators.length && operators[operators.length - 1] !== '(') {
+          output.push(operators.pop());
+        }
+        operators.pop();
+      } else if (precedence[char] !== undefined) {
+        while (operators.length && precedence[operators[operators.length - 1]] >= precedence[char]) {
+          output.push(operators.pop());
+        }
+        operators.push(char);
       }
-      operators.push(char);
     }
+  }
+
+  if (numberBuffer) {
+    output.push(numberBuffer);
   }
 
   while (operators.length) {
     output.push(operators.pop());
   }
 
-  return output.join('');
+  return output.join(' ');
 };
 
 const evaluatePostfix = (expression) => {
+  const precedence = { '+': 1, '-': 1, '*': 2, '/': 2 };
   const stack = [];
 
-  for (let char of expression) {
-    if (/\d/.test(char)) {
+  expression.split(' ').forEach(char => {
+    if (/^-?\d+(\.\d+)?$/.test(char)) {
       stack.push(parseFloat(char));
-    } else if (['+', '-', '*'].includes(char)) {
+    } else if (precedence[char] !== undefined) {
       const b = stack.pop();
       const a = stack.pop();
       switch (char) {
         case '+': stack.push(a + b); break;
         case '-': stack.push(a - b); break;
         case '*': stack.push(a * b); break;
+        case '/': stack.push(a / b); break;
       }
     }
-  }
+  });
 
   return stack[0];
 };
 
 const solve = (expression, x) => {
-
-  const replacedExpression = expression.replaceAll('x', x);
-
+  const replacedExpression = expression.replace(/x/g, x);
   const postfixExpression = infixToPostfix(replacedExpression);
-
   return evaluatePostfix(postfixExpression);
 };
--- tests/solve.js
+++ tests/solve.js
@@ -1,12 +1,30 @@
 'use strict';
 
 QUnit.module('Тестируем функцию solve', function () {
-	QUnit.test('solve работает правильно ', function (assert) {
+	QUnit.test('solve работает правильно с односимвольными числами', function (assert) {
 		assert.strictEqual(solve('x + 1', 1), 2);
 		assert.strictEqual(solve('2 + x - 1', 5), 6);
 		assert.strictEqual(solve('2 * x - 1', 5), 9);
 		assert.strictEqual(solve('2 * ( x - 1 )', 5), 8);
 		assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
 		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
+	});
+
+	QUnit.test('solve работает правильно с многосимвольными числами и делением', function (assert) {
+		assert.strictEqual(solve('100 + x', 23), 123);
+		assert.strictEqual(solve('100 / 2 + x', 50), 100);
+		assert.strictEqual(solve('2 * (x + 5)', 10), 30);
+		assert.strictEqual(solve('10 / (x - 5)', 10), 2);
+	});
+
+	QUnit.test('solve корректно обрабатывает вложенные скобки', function (assert) {
+		assert.strictEqual(solve('((5 - x) * (x + 5)) * (2 + 2)', 3), 64);
+		assert.strictEqual(solve('(10 * (x + 1)) / 2', 9), 50);
+	});
+
+	QUnit.test('solve работает с пограничными значениями', function (assert) {
+		assert.strictEqual(solve('x / 0', 5), Infinity);
+		assert.strictEqual(solve('x + 0', 0), 0);
+		assert.strictEqual(solve('x - 0', -5), -5);
 	});
 });
