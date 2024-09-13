f9eed776cbeb47f63eea6d61d4aa7cff0936214e
Alisa <askerrr2002@gmail.com>
2024-09-13 23:24:24 +0300

    Добавила директиву 'use strict' и документацию

--- source/plain.js
+++ source/plain.js
@@ -1,2 +1,10 @@
+'use strict'; 
+
+/** Принимает на вход массив массивов любой вложенности и создает из них один общий массив.
+ * @param {object[]} arrrays Массив массивов
+ * @throws {TypeError} Если arrays это не массив
+ * @throws {TypeError} Если хотя бы один из элементов массива arrays не является массивом
+ * @returns {object[]} Объединенный массив
+ */
 
 const plain = arrays => arrays.flat(Infinity);
