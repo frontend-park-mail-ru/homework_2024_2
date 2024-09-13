efad421df5afffd8f6f9784f0e3f6eb031b92d71
Ivan Ivashkin <f3shok@gmail.com>
2024-09-13 12:56:28 +0300

    файлы отформатированны

--- source/roman.js
+++ source/roman.js
@@ -1,62 +1,76 @@
-'use strict';
+"use strict";
 
-const DIGITS = {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1};
-
+const DIGITS = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
 
 /**
  * Older function - converter to roman -> decimal, decimal -> roman numbers. Analyzes incoming data and calls function with it properties.
- * @param {string} num 
+ * @param {string} num  - Done value
+ * @throws {TypeError} If number format not decimal or roman
  * @returns string
  */
 function roman(num) {
+  if (/^[1-9]\d*$/.test(num)) {
+    return decimalToRoman(num);
+  }
 
-    if (/^[1-9]\d*$/.test(num)) {
-       return decimalToRoman(num);
-    }
-    
-    if (/^[MDCLXVImdclxvi]+$/.test(num)) {
-       return romanToDecimal(num);
-    }
+  if (/^[MDCLXVImdclxvi]+$/.test(num)) {
+    return romanToDecimal(num);
+  }
 
-    throw new Error("Bad data type")
+  throw new Error("Bad data type");
 }
 
 /**
  * Converts decimal number to roman number
- * @param {string} num 
+ * @param {string} num  - Done value in roman format
+ * @throws {TypeError} If roman number has incorrect format
  * @returns string
  */
 function romanToDecimal(num) {
-    num = num.toUpperCase();
-    let result = 0;
+  num = num.toUpperCase();
+  let result = 0;
 
-    for (let i = 0; i < num.length; i++) {
-        const firstNum = DIGITS[num[i]];
-        const secNum = DIGITS[num[i + 1]] || 0;
-        const thirdNum = DIGITS[num[i + 2]] || 0;
+  for (let i = 0; i < num.length; i++) {
+    const firstNum = DIGITS[num[i]];
+    const secNum = DIGITS[num[i + 1]] || 0;
+    const thirdNum = DIGITS[num[i + 2]] || 0;
 
-        if (secNum && thirdNum && firstNum <= secNum && secNum < thirdNum) {
-            throw new Error ("Bad roman number format");
-        }
-
-        result += secNum > firstNum ? -firstNum : firstNum;
+    if (secNum && thirdNum && firstNum <= secNum && secNum < thirdNum) {
+      throw new Error("Bad roman number format");
     }
 
-    return result;
+    result += secNum > firstNum ? -firstNum : firstNum;
+  }
+
+  return result;
 }
 
 /**
  * Converts roman number to decimal numver
- * @param {string} num 
+ * @param {string} num - Done value in decimal format
  * @returns string
  */
 function decimalToRoman(num) {
-    let result = "";
-    for (let symb in DIGITS) {
-        while (num >= DIGITS[symb]) {
-            result += symb;
-            num -= DIGITS[symb];
-        }
+  let result = "";
+  for (let symb in DIGITS) {
+    while (num >= DIGITS[symb]) {
+      result += symb;
+      num -= DIGITS[symb];
     }
-    return result;
+  }
+  return result;
 }
--- tests/roman.js
+++ tests/roman.js
@@ -1,52 +1,103 @@
-'use strict';
+"use strict";
 
-QUnit.module('Тестируем функцию roman', function () {
-	QUnit.test('roman правильно переводит из римской системы счисления', function (assert) {
-		assert.strictEqual(roman('I'), 1);
-		assert.strictEqual(roman('V'), 5);
-		assert.strictEqual(roman('M'), 1000);
-		assert.strictEqual(roman('l'), 50);
-		assert.strictEqual(roman('d'), 500);
+QUnit.module("Тестируем функцию roman", function () {
+  QUnit.test(
+    "roman правильно переводит из римской системы счисления",
+    function (assert) {
+      assert.strictEqual(roman("I"), 1);
+      assert.strictEqual(roman("V"), 5);
+      assert.strictEqual(roman("M"), 1000);
+      assert.strictEqual(roman("l"), 50);
+      assert.strictEqual(roman("d"), 500);
 
-		assert.strictEqual(roman('iv'), 4);
-		assert.strictEqual(roman('iiii'), 4);
-		assert.strictEqual(roman('CM'), 900);
+      assert.strictEqual(roman("iv"), 4);
+      assert.strictEqual(roman("iiii"), 4);
+      assert.strictEqual(roman("CM"), 900);
 
-		assert.strictEqual(roman('MCMIV'), 1904);
-		assert.strictEqual(roman('MCMXC'), 1990);
-		assert.strictEqual(roman('mmxvii'), 2017);
-	});
+      assert.strictEqual(roman("MCMIV"), 1904);
+      assert.strictEqual(roman("MCMXC"), 1990);
+      assert.strictEqual(roman("mmxvii"), 2017);
+    },
+  );
 
-	QUnit.test('roman правильно переводит из десятичной системы счисления', function (assert) {
-		assert.strictEqual(roman(1), 'I');
-		assert.strictEqual(roman(5), 'V');
-		assert.strictEqual(roman(1000), 'M');
-		assert.strictEqual(roman(50), 'L');
-		assert.strictEqual(roman(500), 'D');
+  QUnit.test(
+    "roman правильно переводит из десятичной системы счисления",
+    function (assert) {
+      assert.strictEqual(roman(1), "I");
+      assert.strictEqual(roman(5), "V");
+      assert.strictEqual(roman(1000), "M");
+      assert.strictEqual(roman(50), "L");
+      assert.strictEqual(roman(500), "D");
 
-		assert.strictEqual(roman(4), 'IV');
-		assert.strictEqual(roman(900), 'CM');
+      assert.strictEqual(roman(4), "IV");
+      assert.strictEqual(roman(900), "CM");
 
-		assert.strictEqual(roman(1904), 'MCMIV');
-		assert.strictEqual(roman(1990), 'MCMXC');
-		assert.strictEqual(roman(2017), 'MMXVII');
-	});
+      assert.strictEqual(roman(1904), "MCMIV");
+      assert.strictEqual(roman(1990), "MCMXC");
+      assert.strictEqual(roman(2017), "MMXVII");
+    },
+  );
 
-	QUnit.test('roman правильно определяет, что было передано на вход', function (assert) {
-		assert.strictEqual(roman('1904'), 'MCMIV');
-		assert.strictEqual(roman('1990'), 'MCMXC');
-		assert.strictEqual(roman('2017'), 'MMXVII');
-	});
+  QUnit.test(
+    "roman правильно определяет, что было передано на вход",
+    function (assert) {
+      assert.strictEqual(roman("1904"), "MCMIV");
+      assert.strictEqual(roman("1990"), "MCMXC");
+      assert.strictEqual(roman("2017"), "MMXVII");
+    },
+  );
 
-    QUnit.test('roman проводит проверку корректности числа записанного римскими цифрами', function (assert) {
-		assert.throws(function() { roman('XLM'); }, Error, "Ошибка выбрасывается для 'XLM'");
-		assert.throws(function() { roman('IVX'); }, Error, "Ошибка выбрасывается для 'IVX'");
-    	assert.throws(function() { roman('CDM'); }, Error, "Ошибка выбрасывается для 'CDM'");
-	});
+  QUnit.test(
+    "roman проводит проверку корректности числа записанного римскими цифрами",
+    function (assert) {
+      assert.throws(
+        function () {
+          roman("XLM");
+        },
+        Error,
+        "Ошибка выбрасывается для 'XLM'",
+      );
+      assert.throws(
+        function () {
+          roman("IVX");
+        },
+        Error,
+        "Ошибка выбрасывается для 'IVX'",
+      );
+      assert.throws(
+        function () {
+          roman("CDM");
+        },
+        Error,
+        "Ошибка выбрасывается для 'CDM'",
+      );
+    },
+  );
 
-	QUnit.test('roman проводит проверку корректности записи числа', function (assert) {
-		assert.throws(function() { roman('00111'); }, Error, "Ошибка выбрасывается для '00111'");
-		assert.throws(function() { roman('19_2'); }, Error, "Ошибка выбрасывается для '19_2'");
-    	assert.throws(function() { roman('ЧXLV'); }, Error, "Ошибка выбрасывается для 'ЧXLV'");
-	});
+  QUnit.test(
+    "roman проводит проверку корректности записи числа",
+    function (assert) {
+      assert.throws(
+        function () {
+          roman("00111");
+        },
+        Error,
+        "Ошибка выбрасывается для '00111'",
+      );
+      assert.throws(
+        function () {
+          roman("19_2");
+        },
+        Error,
+        "Ошибка выбрасывается для '19_2'",
+      );
+      assert.throws(
+        function () {
+          roman("ЧXLV");
+        },
+        Error,
+        "Ошибка выбрасывается для 'ЧXLV'",
+      );
+    },
+  );
 });
