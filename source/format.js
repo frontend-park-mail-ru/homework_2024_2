ff1a280fc8c327649f02d97b942e712ee860a261
mod22k053 <muzalevod@student.bmstu.ru>
2024-09-14 00:49:01 +0300

    prom2

--- source/format.js
+++ source/format.js
@@ -14,35 +14,6 @@
  * @example
  * const formatedString = format([1, 2, 10, 100, 1000, 10000], 6)
  */
-const format1 = (input, columns) => {
-	if (columns < 1) {
-		throw new Error(`Amount of columns could't be ${columns}`)
-	}
-	if (columns > input.length) {
-		throw new Error(`Amount of columns could't be bigger than amount of numbers (${columns}>${input.length})`)
-	}
-	let output = Array(Math.ceil(input.length / columns)).fill("");
-	for (let j = 0; j < columns; j++) {
-		let tmpList = []
-		let maxLen = 0
-		for (let i = j; i < input.length; i += columns) {
-			if (typeof (input[i]) !== "number") {
-				throw new TypeError(`${typeof (input[i])} is in input data, all elements must be numbers`)
-			}
-			tmpList.push(input[i])
-			if (String(input[i]).length > maxLen) {
-				maxLen = String(input[i]).length
-			}
-		}
-		tmpList.forEach((element, iter) => {
-			output[iter] += (output[iter] == "" ? "" : " ")
-				+ String(element).padStart(maxLen)
-		});
-	}
-
-	return output.join("\n")
-}
-
 const format = (input, columns) => {
 	if (columns < 1) {
 		throw new Error(`Amount of columns could't be ${columns}`)
@@ -50,29 +21,39 @@ const format = (input, columns) => {
 	if (columns > input.length) {
 		throw new Error(`Amount of columns could't be bigger than amount of numbers (${columns}>${input.length})`)
 	}
-	let maxOfColumns = Array(columns).fill(0);
-	let columsArr = []
-	const some = (element, iter) => {
-		if (typeof (element) !== "number") {
-			throw new TypeError(`${typeof (element)} is in input data, all elements must be numbers`)
-		}
-		if (String(element).length > maxOfColumns[iter%columns]){
-			maxOfColumns[iter%columns] = String(element).length
-		}
-		if (iter%columns>=columsArr.length){
-			columsArr.push([element])
-		} else {
-			columsArr[iter%columns].push(element)
-		}
+	//проверяем правильность типов, если будет объкт Number его тип все равно будет object и не подойдет
+	let wrong_type = ""
+	input.forEach((element) => {typeof(element)!=="number" ? wrong_type=typeof(element):0})
+	if (wrong_type!==""){
+		throw new TypeError(`${wrong_type} is in input data, all elements must be numbers`)
 	}
-	input.forEach((element, iter) => {try{some(element, iter)}catch(e){throw e}})
-	console.log(maxOfColumns)
-	columsArr.forEach((someColumn, iterColumn) => {columsArr[iterColumn].forEach((element, iter)=> {columsArr[iterColumn][iter]=String(element).padStart(maxOfColumns[iterColumn])} )})
-	for (let j = 0; j < columsArr[0].length; j++){
-		for (let i = 1; i < columsArr.length; i++){
-			columsArr[0][j]+=(columsArr[i][j]!==undefined ?" "+columsArr[i][j] : "")
-		}
-	}
+	//находим необходимую ширину каждой колонки
+	const maxOfColumns = Array(columns).fill(0);
+	input.forEach((element, iter) => {
+		((elementLength, columnNomber)=>{
+			maxOfColumns[columnNomber] = elementLength>maxOfColumns[columnNomber]?elementLength:maxOfColumns[columnNomber]
+		})(String(element).length, iter%columns)
+	})
+	//формируем двумерный массив колонок
+	const columsArr = []
+	input.forEach((element, iter) => {
+		((element, columnNomber)=>{
+			if (columnNomber>=columsArr.length){
+				columsArr.push([element])
+			} else {
+				columsArr[columnNomber].push(element)
+			}
+		})(element, iter%columns)
+	})
+	// для добавляем ведущие пробелы каждому элементу в колонке
+	columsArr.forEach((someColumn, iterColumn) => {
+		columsArr[iterColumn].forEach((element, iter)=> {columsArr[iterColumn][iter]=String(element).padStart(maxOfColumns[iterColumn])
+		} )})
+	// добавляем к элементам первой колонки все остальные элементы
+	columsArr[0].forEach((_, iter1)=>{
+		columsArr.slice(1,columsArr.length).forEach((element, iter2) => {
+			columsArr[0][iter1]+=(element[iter1]!==undefined ?" "+element[iter1] : "")
+		})})
+	//склеиваем первую колонку в которой теперь есть все готовые строки в итоговый ответ
 	return columsArr[0].join("\n")
 }
-
