f3c9be09eaf5d65c67ef63fed6a90f770577983c
Ivan Ivashkin <f3shok@gmail.com>
2024-09-13 11:54:10 +0300

    /source/roman.js выкидывает ошибку при некорректном вводе данных

--- source/roman.js
+++ source/roman.js
@@ -2,6 +2,13 @@
 
 const DIGITS = {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1};
 
+
+/**
+ * Older function - converter to roman -> decimal, decimal -> roman numbers. Analyzes incoming data and calls function with it properties.
+ * @constructor
+ * @param {string} num 
+ * @returns string
+ */
 function roman(num) {
     let res = "";
     if (/^\d+$/.test(num)) {
@@ -9,12 +16,18 @@ function roman(num) {
     } else if (/^[MDCLXVImdclxvi]+$/.test(num)) {
         res = romanToDecimal(num);
     } else {
-        res = "Bad number format"
+        throw new Error("Bad data type")
     }
 
-    return res
+    return res;
 }
 
+/**
+ * Converts decimal number to roman number
+ * @constructor
+ * @param {string} num 
+ * @returns string
+ */
 function romanToDecimal(num) {
     num = num.toUpperCase();
     let result = 0;
@@ -25,7 +38,7 @@ function romanToDecimal(num) {
         const thirdNum = DIGITS[num[i + 2]] || 0;
 
         if (secNum && thirdNum && firstNum <= secNum && secNum < thirdNum) {
-            return ('Bad roman number format');
+            return ("Bad roman number format");
         }
 
         result += secNum > firstNum ? -firstNum : firstNum;
@@ -34,6 +47,12 @@ function romanToDecimal(num) {
     return result;
 }
 
+/**
+ * Converts roman number to decimal numver
+ * @construcor
+ * @param {string} num 
+ * @returns string
+ */
 function decimalToRoman(num) {
     let res = "";
     for (let symb in DIGITS) {
