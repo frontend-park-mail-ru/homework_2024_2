95cc5af45a4c0230a493e902cb760c4891110d32
Alexander Novak <shura.novak.03@mail.ru>
2024-09-14 19:12:09 +0300

    added new tests, modified function and made JsDoc

--- source/solve.js
+++ source/solve.js
@@ -1,38 +1,58 @@
 'use strict';
+/**
+ * Принимает строку, парсит ее и возвращает массив, состоящий из строк представляющих число,
+ * скобок и знаков
+ * @param {string} str - Строка, которую необходимо парсить
+ * @returns {string[]} - Результат парсинга строки (массив знаков, скобок и чисел)
+ * @throws {TypeError} - В строке вместо чисел операции проводятся над другими символами
+ */
 const parseStr = (str) => {
     const res = []; 
+    let sign = 1;
     let curNum = -1;
-    for (let elem of str) {
-        switch (elem) {
-            case '+':
-            case '-':
-            case '*':
-            case '(':
-            case ')':
-                if (curNum != -1) {
-                    res.push(String(curNum));
+    //здесь if с includes, а не switch, т.к небольшое количество ветвлений if
+    for (let i = 0; i < str.length; i++) {
+        if (['+', '*', '(', ')'].includes(str[i])){
+            if (curNum !== -1) {
+                res.push(String(sign * curNum));
+                sign = 1;
+            }
+            curNum = -1;
+            res.push(str[i]);
+        //отдельно случай с минусом тк он может быть унарным и бинарным
+        }else if (str[i] === '-'){
+            if (i + 1 < str.length && str[i + 1] === ' '){
+                if (curNum !== -1) {
+                    res.push(String(sign * curNum));
+                    sign = 1;
                 }
                 curNum = -1;
-                res.push(elem);
-                break;
-            case ' ':
-                break;
-            default:
-                if (!isNaN(parseInt(elem))){
-                    curNum = curNum === -1 ? Number(elem) : curNum * 10 + Number(elem);  
-                }else{
-                    throw new TypeError("Тип должен быть числовым!");
-                }
+                res.push(str[i]);
+            }else{
+                sign *= -1;
+            }
+        }else if (str[i] !== ' '){
+            if (!isNaN(parseInt(str[i]))){
+                curNum = curNum === -1 ? Number(str[i]) : curNum * 10 + Number(str[i]);  
+            }else{
+                throw new TypeError("Тип должен быть числовым!");
+            }
         }
     }
 
     if (curNum !== -1) {
-        res.push(String(curNum));
+        res.push(String(sign * curNum));
     }
-
+    console.log(res);
     return res;
 }
-
+/**
+ * Принимает массив строк, полученный в результате функции parseStr, представляющий выражение 
+ * и переводит это выражение в обратную польскую запись
+ * @param {string[]} str - Массив представляющий выражение, которое нужно перевести в обратную польскую запись 
+ * @returns {string[]} - Массив представляющий выражение переведенное в обратную польскую запись
+ * @throws {Error} - Если в выражении неправильно расставлены скобки
+ */
 const toPolish = (str) => {
     const masStr = parseStr(str); 
     const priorities = {
@@ -43,8 +63,8 @@ const toPolish = (str) => {
     const myStack = [];
     const answer = [];
 
-    
     for (let val of masStr) {
+        //if так как неудобно писать switch (много однотипных case)
         if (['+', '-', '*'].includes(val)) {
             while (priorities[val] <= priorities[myStack.at(-1)]){
                 answer.push(myStack.pop());
@@ -53,8 +73,11 @@ const toPolish = (str) => {
         }else if (val === '('){
             myStack.push(val);
         }else if (val === ')'){
-            while (myStack.at(-1) !== '('){
+            while (myStack.length > 0 && myStack.at(-1) !== '('){
                 answer.push(myStack.pop());
+            }
+            if (myStack.length == 0){
+                throw new Error("Неправильно расставлены скобки !!!");
             }
             myStack.pop();
         }else if (val !== ' '){
@@ -62,22 +85,33 @@ const toPolish = (str) => {
         }
     }
 
+    console.log(myStack)
     while(myStack.length !== 0){
-        answer.push(myStack.pop());
+        const elem = myStack.pop();
+        if (['(', ')'].includes(elem)){
+            throw new Error("неправильно расставлены скобки !!!");
+        }
+        answer.push(elem);
     }
-    
+    console.log(answer);
     return answer;
 }
-
+/**
+ * Принимает на вход массив представляющий выражение в обратной польской записи и решает это выражение
+ * @param {string[]} str - Массив представляющий выражение, которое нужно решить
+ * @returns {number} - Результат вычисления выражения
+ */
 const solvePolish = (str) => {
     const myStack = [];
 
     for (let elem of str) {
+        //здесь switch, тк много ветвлений
         switch(elem) {
             case '+':
                 myStack.push(myStack.pop() + myStack.pop());
                 break;
             case '-':
+                //обработка случая с унарным минусом в начале строки
                 const oper1 = myStack.pop();
                 const oper2 = myStack.pop();
                 myStack.push(oper2 - oper1);
@@ -95,17 +129,26 @@ const solvePolish = (str) => {
 
 }
 
-
+/**
+ * Принимает строку представляющую выражение, которое нужно решить, и число, которое нужно подставить вместо x
+ * Находит результат выражения
+ * @param {string} str - Строка представляющая выражение  
+ * @param {number} val - Число которое нужно подставить вместо x 
+ * @returns {number} - Результат вычисления выражения
+ * @throws {Error} - Если в функцию передали не все параметры
+ * @throws {TypeError} - Если в функцию передали параметры других типов
+ */
 const solve = (str, val) => {
+    // вначале обработка ошибок в конце логика 
     if (str === undefined || val === undefined) {
         throw new Error("str and val arguments must not be empty");
-    }
-    if (typeof str === "string" && typeof val === "number" ){
-        return solvePolish(toPolish(str.replaceAll('x', val)));
-    }else{
+    }else if (typeof str !== "string" || typeof val !== "number") {
         throw new TypeError("argument str must be string, argument val must be int");
+    }else{
+        return solvePolish(toPolish(str.replaceAll('x', val)));
     }
 }
+
 
 
 
--- tests/solve.js
+++ tests/solve.js
@@ -12,9 +12,32 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('(x - 5) * (x + 5 * 5) - x', 10), 165);
 		assert.strictEqual(solve('(x + x + x) * (x * (x * (x + x)))', 2), 96);
 		assert.strictEqual(solve('x * (x + x) - (x * (x + x))', 5), 0);
+	});
+
+	QUnit.test('Работает правильно при многосимвольных числах', function (assert) {
+		assert.strictEqual(solve('(125 + 125) * (230 - 226) + x', 125), 1125);
+		assert.strictEqual(solve('(-199999 + 199995) * 100 + 555', 155), 155);
+		assert.strictEqual(solve('(5555 - x) * x + (x - 4500)', 5554), 6608);
+	});
+
+	QUnit.test('Работает правильно при отрицательных числах', function (assert) {
+		assert.strictEqual(solve('-5 * (-5 + -5) - -25 - -10', 85), 85);
+		assert.strictEqual(solve('-5 * (6 * -5) + -25 + x', -25), 100);
+		assert.strictEqual(solve('-5 - -5 * (-5 + 6 + -5) - 5', -30), -30);
+	});
+
+	QUnit.test('Работает правильно при неправильно расставленных скобках ', function (assert) {
+		assert.throws(() => solve('(5 + 6))* (7 + 8)', 10), Error);
+		assert.throws(() => solve('(5 + 7 * 8', 25), Error);
+		assert.throws(() => solve('(5 + 7 ( + )8 * 9(', -30), Error);
+	});
+
+	QUnit.test('Работает правильно при неправильных типах параметров ', function (assert) {
 		assert.throws(() => solve('x * (x + x) - (bb * (x + x))', 6), TypeError);
 		assert.throws(() => solve('x * (x + x) - (x * (x / y))', 6), TypeError);
 		assert.throws(() => solve(7, 'jhxhe'), TypeError);
 		assert.throws(() => solve(), Error);
 	});
 });
+
+
