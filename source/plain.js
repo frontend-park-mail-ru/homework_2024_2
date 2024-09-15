e256379a230c7c23543069077bb3e18d004f561b
Alisa <askerrr2002@gmail.com>
2024-09-15 17:50:09 +0300

    Добавлена предварительная проверка на TypeError, исправлена документация, пробел и перевод строки

--- source/plain.js
+++ source/plain.js
@@ -1,13 +1,18 @@
 'use strict'; 
 
 /** Принимает на вход массив массивов любой вложенности и создает из них один общий массив.
- * @param {object[]} arrays Массив массивов
+ * @param {any[]} arrays Массив массивов
+ * @throws {TypeError} Если arrays это не массив
+ * @returns {any[]} Объединенный массив
  * @example
  * const array = [1, [null, [3]], ['четыре']]
  * const joint_array = plain(array)
  * console.log(joint_array); // Вывод: [1, null, 3, 'четыре']
- * @throws {TypeError} Если arrays это не массив
- * @returns {object[]} Объединенный массив
  */
-
-const plain = arrays => arrays.reduce((arr, i) => Array.isArray(i) ? arr.concat(plain(i)) : arr.concat(i), []);
+const plain = arrays => {
+    if (!Array.isArray(arrays)){
+        throw new TypeError('Введен не массив');
+    };
+    
+    return arrays.reduce((arr, i) => arr.concat(Array.isArray(i) ? plain(i) : i), [])
+};
