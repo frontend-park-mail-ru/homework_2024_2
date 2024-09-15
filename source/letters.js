f123eb8aeaf274785eabc32797e37e2eb9d6cda4
Asman Mamou <asman2003pda45@gmail.com>
2024-09-15 14:47:18 +0300

    Мамоу Асман вар-10 Исправления

--- source/letters.js
+++ source/letters.js
@@ -4,23 +4,21 @@
  * Если `keepFirst` равно `false`, оставляет только последнее вхождение повторяющихся символов.
  * Если `keepFirst` не передан, удаляет все повторяющиеся символы.
  *
- * @param {*} str - Исходная строка.
+ * @param {*} str - Исходное значение. Ожидается строка или объект String.
  * @param {boolean} [keepFirst] - Флаг, определяющий, сохранять ли первое вхождение (`true`) или последнее (`false`). Если не указан, удаляются все повторяющиеся символы.
  * @returns {string} - Обработанная строка без повторяющихся символов в соответствии с указанным флагом.
- * @throws {TypeError} - Если `str` не является строкой или не имеет свойства `length`.
+ * @throws {TypeError} - Если `str` не является строкой или объектом String.
  */
 function letters(str, keepFirst) {
-    if (typeof str !== 'string') {
-        throw new TypeError(`Ожидалась строка, но получен тип ${typeof str}`);
+    if (typeof str !== 'string' && !(str instanceof String)) {
+        throw new TypeError(`Ожидалась строка или объект String, но получен тип ${typeof str}.`);
     }
 
-    if (typeof str.length !== 'number') {
-        throw new TypeError('Входной объект должен иметь свойство length типа number');
+    if (!str?.length) return '';
+
+    if (str instanceof String) {
+        str = str.valueOf();
     }
-
-    if (str.length === 0) return '';
-
-    let result = [];
 
     if (keepFirst === undefined) {
         const charCount = str.split('').reduce((acc, char) => {
@@ -28,26 +26,28 @@ function letters(str, keepFirst) {
             return acc;
         }, {});
 
-        result = str.split('').filter(char => charCount[char] === 1);
+        const result = str.split('').filter(char => charCount[char] === 1);
+        return result.join('');
     }
-    else if (keepFirst) {
+
+    if (keepFirst) {
         const seen = new Set();
-        const chars = Array.from(str);
-        result = chars.filter(char => {
+        const result = Array.from(str).filter(char => {
             if (!seen.has(char)) {
                 seen.add(char);
                 return true;
             }
             return false;
         });
-    } else {
-        const lastIndex = {};
-        str.split('').forEach((char, index) => {
-            lastIndex[char] = index;
-        });
-
-        result = str.split('').filter((char, index) => lastIndex[char] === index);
+        return result.join('');
     }
 
+    const lastIndex = {};
+    str.split('').forEach((char, index) => {
+        lastIndex[char] = index;
+    });
+
+    const result = str.split('').filter((char, index) => lastIndex[char] === index);
     return result.join('');
+
 }
