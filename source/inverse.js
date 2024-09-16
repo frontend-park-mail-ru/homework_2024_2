10aba0c268f96c80c3ee1cc68415df0969829701
Ifelsik <misha2004546@gmail.com>
2024-09-16 19:24:59 +0300

    remade with ternary operator

--- source/inverse.js
+++ source/inverse.js
@@ -15,9 +15,9 @@ const inverse = (array, shift = 0) => {
     if (!Array.isArray(array) || !Number.isInteger(shift)) {
         throw new TypeError('Incorrect types of argument!');
     }
-    if (shift >= 0) {  // The shift is positive or zero so the second part of aray should be inverted 
-        return [...array.slice(0, shift), ...array.slice(shift).reverse()];
-    } else {  // Other case. Inverting first part of array
-        return [...array.slice(0, shift).reverse(), ...array.slice(shift)];
-    }
+    return shift >= 0
+             // The shift is positive or zero so the second part of aray should be inverted 
+           ? [...array.slice(0, shift), ...array.slice(shift).reverse()] 
+             // Other case. Inverting first part of array
+           : [...array.slice(0, shift).reverse(), ...array.slice(shift)];
 };
