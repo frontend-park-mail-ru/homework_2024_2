33f2ceec99b4cbcb8b5e69fecbae61ac6b1691f6
AnGLiHaNiN <91965947+Qaleka@users.noreply.github.com>
2024-09-16 14:08:28 +0300

    mb final version

--- source/filter.js
+++ source/filter.js
@@ -35,10 +35,10 @@ const filter = (html, allowedTags) => {
 
 		if (allowedTags.includes(tagName)) {
 			const originalTag = findTag.replace(/&lt;/g, '<')
-									.replace(/&gt;/g, '>')
-									.replace(/&quot;/g, '"')
-									.replace(/&#39;/g, '\'')
-									.replace(/&amp;/g, '&');
+							.replace(/&gt;/g, '>')
+							.replace(/&quot;/g, '"')
+							.replace(/&#39;/g, '\'')
+							.replace(/&amp;/g, '&');
 			
 			return originalTag;
 		}
