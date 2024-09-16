7c1a75c06e6838b56187e834692c1fb63996f59a
qosmioo <79136983+qosmioo@users.noreply.github.com>
2024-09-16 19:45:23 +0300

    fix codestyle.

--- source/format.js
+++ source/format.js
@@ -21,7 +21,7 @@ const format = (input, columns) => {
         if (typeof val !== 'number') {
             throw new TypeError('Input data must contain integers only');
         }
-    })
+    });
 
     const widths = Array(columns).fill(0).reduce((acc, _, index) => {
         acc[index] = Math.max(...input.filter((_, j) => j % columns === index % columns)
@@ -30,9 +30,9 @@ const format = (input, columns) => {
     }, []);
 
     return input.reduce((acc, val, i) => {
-        const currColumn = i % columns
+        const currColumn = i % columns;
         acc += val.toString().padStart(widths[currColumn]);
-        
+
         if (i % columns === columns - 1 && i !== input.length - 1) {
             acc += '\n';
         } else if (i !== input.length - 1) {
