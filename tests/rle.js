b567887ad8edc540b1a8779115eb750fa4e94955
liya <omarovaam@bmstu.ru>
2024-09-16 22:15:31 +0300

    fix margins

--- tests/rle.js
+++ tests/rle.js
@@ -1,30 +1,29 @@
 'use strict';
 
 QUnit.module('Тестируем функцию rle', function () {
-	QUnit.test('rle работает правильно', function (assert) {
-		assert.strictEqual(rle('AAAB'), 'A3B');
-		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
-		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
-	});
+    QUnit.test('rle работает правильно', function (assert) {
+        assert.strictEqual(rle('AAAB'), 'A3B');
+        assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
+        assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
+    });
 
-	QUnit.test('Корректная работа с одиночными символами', function (assert) {
-		assert.strictEqual(rle('ABCDE'), 'ABCDE', 'rle("ABCDE") === "ABCDE"');
-		assert.strictEqual(rle('ABCD'), 'ABCD', 'rle("ABCD") === "ABCD"');
-	});
+    QUnit.test('Корректная работа с одиночными символами', function (assert) {
+        assert.strictEqual(rle('ABCDE'), 'ABCDE', 'rle("ABCDE") === "ABCDE"');
+        assert.strictEqual(rle('ABCD'), 'ABCD', 'rle("ABCD") === "ABCD"');
+    });
 
-	QUnit.test('Корректная работа со строками, содержащими только один символ', function (assert) {
-		assert.strictEqual(rle('BBBBBBB'), 'B7', 'rle("BBBBBBB") === "B7"');
-    	assert.strictEqual(rle('CCCCCCCC'), 'C8', 'rle("CCCCCCCC") === "C8"');
-	});
+    QUnit.test('Корректная работа со строками, содержащими только один символ', function (assert) {
+        assert.strictEqual(rle('BBBBBBB'), 'B7', 'rle("BBBBBBB") === "B7"');
+        assert.strictEqual(rle('CCCCCCCC'), 'C8', 'rle("CCCCCCCC") === "C8"');
+    });
 
-	QUnit.test('Ошибка при передаче пустой строки', function (assert) {
-    assert.throws(
-        function() {
-            rle('');
-        },
-        TypeError,
-        'Функция выбрасывает ошибку при передаче пустой строки'
-	);
-	});
-
+    QUnit.test('Ошибка при передаче пустой строки', function (assert) {
+        assert.throws(
+            function() {
+                rle('');
+            },
+            TypeError,
+            'Функция выбрасывает ошибку при передаче пустой строки'
+        );
+    });
 });
