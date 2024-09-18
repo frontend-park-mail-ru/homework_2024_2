d1a91614d5513df0e98deb164656a3524e45826c
Alexander Rudenko <ram181524@gmail.com>
2024-09-18 20:59:00 +0300

    jsdoc added, comment removed

--- source/filter.js
+++ source/filter.js
@@ -1,7 +1,15 @@
 "use strict";
 
+/**
+ * Regular expression to match HTML tags.
+ * @type {RegExp}
+ */
 const TAG_PATTERNS = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi;
 
+/**
+ * A map of special characters to their HTML entity equivalents.
+ * @type {Object.<string, string>}
+ */
 const SPECIAL_CHARS = {
 	"&": "&amp;",
 	"<": "&lt;",
@@ -9,27 +17,36 @@ const SPECIAL_CHARS = {
 	'"': "&quot;",
 	"'": "&#39;",
 };
+
+/**
+ * Escapes special HTML characters in a given string.
+ *
+ * @param {string} text - The input text that may contain special characters.
+ * @returns {string} - The escaped HTML string.
+ */
 const changeHTML = (text) => text.replace(/[&<>"']/g, (char) => SPECIAL_CHARS[char]);
 
-// Основная функция фильтрации
+/**
+ * Filters the input string by escaping HTML tags not included in the allowedTags list.
+ *
+ * @param {string} input - The input string that may contain HTML tags.
+ * @param {string[]} allowedTags - An array of allowed tag names. Tags not in this list will be escaped.
+ * @returns {string} - The filtered string with only allowed tags.
+ */
 const filter = (input, allowedTags) => {
-	// Приводим input к строке или пустой строке, если input = null или undefined
 	input = typeof input === "string" ? input : input == null ? "" : String(input);
 
 	let result = "";
 	let lastIndex = 0;
 
 	input.replace(TAG_PATTERNS, (match, tagName, currentTag) => {
-		// Добавляем текст между последним индексом и текущим тегом
 		result += changeHTML(input.slice(lastIndex, currentTag));
 		lastIndex = currentTag + match.length;
 
-		//экранируем только запрещенные теги
 		result += allowedTags.includes(tagName.toLowerCase()) ? match : changeHTML(match);
 		return match;
 	});
 
-	// Добавляем оставшийся текст после последнего тега
 	result += changeHTML(input.slice(lastIndex));
 
 	return result;
