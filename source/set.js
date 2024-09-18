21522a0357fa5c56a1ad67daa555ceb858a35680
Rassmagin Alexander <rassmagin.wolf@mail.ru>
2024-09-19 02:09:11 +0300

    arrow + no path var

--- source/set.js
+++ source/set.js
@@ -1,16 +1,14 @@
 'use strict';
 
-function set(object, path, value) {
-    const pathArray = path.substring(1).split('.');
-    pathArray.reduce((obj, currentKey, index) => {
-      if (!Object.hasOwn(obj, currentKey)) {
-        obj[currentKey] = {};
-      }
-      if (index == pathArray.length - 1) {
-        obj[currentKey] = value;
-      }
-      return obj[currentKey];
-    }, object);
-    return object;
+const set = (object, path, value) => {
+  path.substring(1).split('.').reduce((obj, currentKey, index) => {
+    if (!Object.hasOwn(obj, currentKey)) {
+      obj[currentKey] = {};
+    }
+    if (index == path.substring(1).split('.').length - 1) {
+      obj[currentKey] = value;
+    }
+    return obj[currentKey];
+  }, object);
+  return object;
   }
-  
